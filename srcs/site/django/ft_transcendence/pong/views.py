95be4bc673961b0c827ff1ff0b3a7560ca361a99
basil <basiljeannot@icloud.com>
2024-10-03 13:06:52 +0200

    pushing

--- srcs/site/django/ft_transcendence/pong/views.py
+++
@@ -1,9 +0,0 @@
-from inspect import getfile
-from django.shortcuts import render
-from django.views import View
-from django.http import HttpResponse
-
-def index(request):
-	# http = getfile("../../../game/frontend/game.html")
-	http = "bite"
-	return HttpResponse(http)
