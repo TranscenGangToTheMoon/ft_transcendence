c5c489182be2786442888873cf5db053092732b7
fguirama <florian.guiramand@protonmail.com>
2024-10-06 00:00:22 +0200

    update api_test.py script (client testing REST api)

--- django/api_test.py
+++ django/api_test.py
@@ -1,3 +1,8 @@
 import requests
 
-print(requests.get("http://127.0.0.1:8000/api?product_id=234").text)
+
+data = {"username": "courgrgcoulekip", 'password': 'blanlablo', "trophy": 3443}
+
+create = requests.post("http://127.0.0.1:8000/api/users/").json()
+print(create)
+print(requests.put(f"http://127.0.0.1:8000/api/users/{create['id']}/", json=data).json())
