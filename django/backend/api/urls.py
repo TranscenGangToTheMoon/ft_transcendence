4c0d50cba64f196f74e4e06945fb819c2c595f41
fguirama <florian.guiramand@protonmail.com>
2024-10-06 00:01:28 +0200

    update api root path

--- django/backend/api/urls.py
+++ django/backend/api/urls.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
-from django.urls import path
+from django.urls import path, include
 
 from api.views import api_root
 
 urlpatterns = [
-    path('', api_root, name="main"),
-    path('user/', api_root, name="main"),
+    path('', api_root, name="api-index"),
+    path('users/', include('users.urls'), name="api-users"),
+    path('auth/', include('rest_framework.urls'), name="api-auth"),
+    # path('games/', include('games.urls'), name="api-games"),
 ]
