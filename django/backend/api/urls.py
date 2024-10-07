f7204836f3d31b801d7369a4c78b4512bb5b6e6a
fguirama <florian.guiramand@protonmail.com>
2024-10-07 22:54:07 +0200

    add auth token endpoint /api/auth

--- django/backend/api/urls.py
+++ django/backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 from api.views import api_root
 
 urlpatterns = [
     path('', api_root, name="api-index"),
     path('users/', include('users.urls'), name="api-users"),
-    path('auth/', include('rest_framework.urls'), name="api-auth"),
+    path('auth/', obtain_auth_token, name="api-auth"),
     # path('games/', include('games.urls'), name="api-games"),
 ]
