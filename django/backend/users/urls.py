ffdec448beab67fc86d51d1f59b1646504a0beed
fguirama <florian.guiramand@protonmail.com>
2024-10-09 20:54:47 +0200

    add search url for user

--- django/backend/users/urls.py
+++ django/backend/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from users.views import users_retrieve_update_delete_view, users_list_create_view, users_retrieve_view
+from users.views import users_retrieve_update_delete_view, users_list_create_view, users_retrieve_view, \
+    UsersSearchListView
 
 urlpatterns = [
     path('', users_list_create_view, name="users-list"),
     path('<int:pk>/', users_retrieve_view, name="users-detail"),
     path('me/', users_retrieve_update_delete_view, name="users-me"),
+    path('search/', UsersSearchListView.as_view(), name="users-search"),
 ]
