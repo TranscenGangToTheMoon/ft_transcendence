064c7611e18f0d8dcfa823faa01ae6ee84de9244
fguirama <florian.guiramand@protonmail.com>
2024-10-09 20:55:00 +0200

    add unique validators for username

--- django/backend/users/validators.py
+++ django/backend/users/validators.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from users.models import Users
 
@@ -12,3 +13,6 @@ def validate_username(value):
     if value is not None:
         value = value.upper()
     return value
+
+
+unique_username = UniqueValidator(queryset=Users, message="Username already exists", lookup="iexact")
