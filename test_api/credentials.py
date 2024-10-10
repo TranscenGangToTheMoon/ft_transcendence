0103c42bfd8827c0fc5c0f9e8877e6bea2804269
fguirama <florian.guiramand@protonmail.com>
2024-10-10 02:12:11 +0200

    add test_api python client
    - Add file credentials.py for making test of all credentials.py

---
+++ test_api/credentials.py
@@ -0,0 +1,47 @@
+import json
+from getpass import getpass
+
+import requests
+
+base_enpoint = 'http://localhost:8000/api/auth/'
+
+
+def ipt_username():
+    return input('enter username -> ')
+
+
+def auth_guest():
+    r = requests.post(base_enpoint + 'guest/')
+    print(r.status_code, r.json())
+    return r.json()
+
+
+def register_guest():
+    access_token = auth_guest()['access']
+    r = requests.post(
+        base_enpoint + 'guest/register/',
+        headers={'Content-Type': 'application/json', 'Authorization': 'Bearer ' + access_token},
+        data=json.dumps({'username': ipt_username(), 'password': getpass()})
+    )
+    print(r.status_code, r.json())
+    return r.json()
+
+
+def register():
+    r = requests.post(
+        base_enpoint + 'register/',
+        headers={'Content-Type': 'application/json'},
+        data=json.dumps({'username': ipt_username(), 'password': getpass()})
+    )
+    print(r.status_code, r.json())
+    return r.json()
+
+
+def get_token():
+    r = requests.post(
+        base_enpoint + 'token/',
+        headers={'Content-Type': 'application/json'},
+        data=json.dumps({'username': ipt_username(), 'password': getpass()})
+    )
+    print(r.status_code, r.json())
+    return r.json()
