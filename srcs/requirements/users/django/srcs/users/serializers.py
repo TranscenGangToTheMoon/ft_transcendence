c85cba2711593128ffb3e67b5a96570148f86464
fguirama <florian.guiramand@protonmail.com>
2024-10-12 04:53:59 +0200

    fix Users field serializers

--- srcs/requirements/users/django/srcs/users/serializers.py
+++ srcs/requirements/users/django/srcs/users/serializers.py
@@ -3,8 +3,26 @@ from rest_framework import serializers
 from users.models import Users
 
 
-class UsersMeSerializer(serializers.ModelSerializer):
+class UsersSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    is_guest = serializers.BooleanField(read_only=True)
+    profile_picture = serializers.IntegerField(read_only=True)
+    coins = serializers.IntegerField(read_only=True)
+    trophy = serializers.IntegerField(read_only=True)
+    current_rank = serializers.IntegerField(read_only=True)
+    accept_friend_request = serializers.BooleanField(write_only=True)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = Users
-        fields = '__all__'
+        fields = [
+            'id',
+            'username',
+            'password',
+            'is_guest',
+            'profile_picture',
+            'coins',
+            'trophy',
+            'current_rank',
+            'accept_friend_request',
+        ]
