1d5c12aebb3a75c358b4d6412bba50fb62b33eff
fguirama <florian.guiramand@protonmail.com>
2024-10-13 03:31:36 +0200

    update user manager

--- srcs/requirements/users/django/srcs/user_management/permissions.py
+++ srcs/requirements/users/django/srcs/user_management/permissions.py
@@ -1,6 +1,7 @@
-from rest_framework import permissions
+from rest_framework import permissions, serializers
 
 from user_management.auth import auth_verify
+from users.models import Users
 
 
 class IsAuthenticated(permissions.BasePermission):
@@ -8,3 +9,18 @@ class IsAuthenticated(permissions.BasePermission):
     def has_permission(self, request, view):
         auth_verify(request.headers.get('Authorization'))
         return True
+
+
+def get_object(request):
+    if request is None:
+        raise serializers.ValidationError({'error': 'Request must be provided.'})
+
+    json_data = auth_verify(request.headers.get('Authorization'))
+
+    try:
+        user = Users.objects.get(id=json_data['id'])
+        if user.is_guest != json_data['is_guest']:
+            user.update(is_guest=json_data['is_guest'])
+        return user
+    except Users.DoesNotExist:
+        return Users.objects.create(**json_data)
--- srcs/requirements/users/django/srcs/user_management/urls.py
+++ srcs/requirements/users/django/srcs/user_management/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 
+from friends.views import friends_view
 from users.views import users_me_view
 
 urlpatterns = [
-    path('api/users/me/', users_me_view, name='users_view'), # todo : DELETE | UPDATE check again the password
-    # path('api/users/me/friends/', users, name='users'), # todo : make GET, POST, UPDATE - get friends, send friend request, accept_friend_request, etc...
-    # path('api/users/me/blocked/', users, name='users'), # todo : make GET, POST, UPDATE
+    path('api/users/me/', users_me_view, name='users_view'),
+    # todo : make two url friend, and friend request
+    #  - GET all friends
+    #  - DELETE friends
+    #  - POST friend request
+    #  - DELETE friend request
+    #  - GET all friend request
+    #  - POST accept friend request
+    path('api/users/me/friends/', friends_view, name='users'),
+    # path('api/users/me/friends/<int:pk>/', friends_view, name='users'), # todo : DELETE
+    # path('api/users/me/blocked/', users, name='users'), # todo : make GET, POST, UPDATE \\ when block remove friend request
     # path('api/users/me/profile_picture/', users, name='users'), # todo : make POST, UPDATE
     # path('api/users/me/online_status/', users, name='users'), # todo : UPDATE is_online game_playing last_online
     # path('api/users/me/coins/', users, name='users'), # todo : UPDATE only transcendence-nw
