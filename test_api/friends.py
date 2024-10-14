4e711017b332a6009b68320bade85432181557dd
fguirama <florian.guiramand@protonmail.com>
2024-10-14 10:15:44 +0200

    update test_api friends

--- test_api/friends.py
+++ test_api/friends.py
@@ -8,24 +8,28 @@ from test_api.credentials import login, register
 token = input('whats token type -> ')
 if token == 'login':
     token = login()['access']
+    copy(token)
 elif token == 'register':
     token = register()['access']
-elif token == 'token':
-    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI4Nzg1NTA0LCJpYXQiOjE3Mjg3ODE5MDQsImp0aSI6IjEyOTcwMTNhOTgzNDQ1ZGZiYTg0MWNjODEwMmQwZTE0IiwidXNlcl9pZCI6MzB9.CXDeWocKyLlafFIxWjMp2iFqshKKwqoomjYjx1CpsPg'
-    # token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI4NzgxODc2LCJpYXQiOjE3Mjg3NzgyNzYsImp0aSI6IjA1NGYxOGUzZjQwNTRkNWNiNThlMDYwOWM5OWE2ODAyIiwidXNlcl9pZCI6MTV9.-SUULh3SpIaLXYPxe5VRxIxJc1oWYYCt6Er0vnB1fU4'
-    # token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI4Nzc5NDg1LCJpYXQiOjE3Mjg3NzU4ODUsImp0aSI6IjFjYmUwMmViMGY3YTRmNDlhNzJiN2UwZjMwZDFkMjYwIiwidXNlcl9pZCI6MzB9.KhAVZyOzpEJszXE3xrFW_peC6eqWsm1-xzFeVhyOAEw'
-else:
+    copy(token)
+elif token == 'paste':
     token = paste()
-copy(token)
+else:#if token == 'token':
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI4ODk0ODgyLCJpYXQiOjE3Mjg4OTEyODIsImp0aSI6IjU5NWVlMDA5OWFjNjQ4YjVhZGI5MzNiZDk1YzQxNGQ1IiwidXNlcl9pZCI6MzR9.nv1vl5ximFxqpLZ7Emrk4BFTqZHNJtt2IH63i2_hyZE'
+# else:
+#     token = paste()
+# copy(token)
 
 
-r = requests.post('http://localhost:8005/api/users/me/friends/', headers={
+r = requests.get('http://localhost:8005/api/users/me/friend_requests/', headers={
     'Authorization': 'Bearer ' + token,
     'Content-Type': 'application/json'
 },
-                  data=json.dumps({'username': 'coucou'}))
+                  data=json.dumps({'username': 'pipi'}))
 
 print(r.status_code)
+if r.status_code == 204:
+    exit()
 print('{')
 for k, v in r.json().items():
     if type(k) is str:
