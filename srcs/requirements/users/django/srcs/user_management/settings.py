71fd9d2174250862d772c1125ba177d1807a60ad
fguirama <florian.guiramand@protonmail.com>
2024-10-14 12:52:36 +0200

    add block endpoint

--- srcs/requirements/users/django/srcs/user_management/settings.py
+++ srcs/requirements/users/django/srcs/user_management/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'block',
     'friend_requests',
     'friends',
     'profile_pictures',
--- srcs/requirements/users/django/srcs/user_management/urls.py
+++ srcs/requirements/users/django/srcs/user_management/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from block.views import block_list_create_view, block_delete_view
 from friend_requests.views import friend_requests_list_create_view, friend_requests_retrieve_delete_view, \
     friend_requests_receive_list_view
 from friends.views import friends_list_create_view, friends_retrieve_delete_view
@@ -14,7 +15,9 @@ urlpatterns = [
     path('api/users/me/friend_requests/receive/', friend_requests_receive_list_view, name='api-friend-request-receive-list'),
     path('api/users/me/friend_requests/<int:pk>/', friend_requests_retrieve_delete_view, name='api-friend-request-retrieve-delete'),
 
-    # path('api/users/me/blocked/', users, name='users'), # todo : make GET, POST, UPDATE \\ when block remove friend request
+    path('api/users/me/block/', block_list_create_view, name='api-block-list-update'),
+    path('api/users/me/block/<int:pk>/', block_delete_view, name='api-block-delete'),
+
     # path('api/users/me/profile_picture/', users, name='users'), # todo : make POST, UPDATE
     # path('api/users/me/online_status/', users, name='users'), # todo : UPDATE is_online game_playing last_online
     # path('api/users/me/coins/', users, name='users'), # todo : UPDATE only transcendence-nw
