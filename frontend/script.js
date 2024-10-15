1482568028f81c5369b5605204cc68481c43f416
limastic <faust.nino@gmail.com>
2024-10-15 19:22:52 +0200

    handled auth endpoints

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,8 @@
 const baseAPIUrl = "http://localhost:8000/api"
 let guestLogin = undefined;
 
-function apiRequest(token, endpoint, callback, method="GET", authType="Bearer", contentType="applications/json"){ 
+function apiRequest(token, endpoint, callback, method="GET", authType="Bearer",
+    contentType="application/json", errorHandling=error=>console.log(error), body=undefined){ 
     let options = {
         method: method,
         headers: {
@@ -10,10 +11,13 @@ function apiRequest(token, endpoint, callback, method="GET", authType="Bearer", 
     }
     if (token)
         options.headers["Authorization"] = `${authType} ${token}`;
+    if (body)
+        options.body = JSON.stringify(body);
+    console.log(endpoint, options)
     fetch(endpoint, options)
         .then(response => response.json())
         .then(data => callback(data))
-        .catch(error => console.log(error))
+        .catch(error => errorHandling(error))
 }
 
 document.addEventListener('DOMContentLoaded', event => {
@@ -26,7 +30,7 @@ document.addEventListener('DOMContentLoaded', event => {
 
 function fillGuestPlaceHolder(){
     const guestLoginField = document.getElementById('guestLoginField');
-    apiRequest(localStorage.getItem('token'), `${baseAPIUrl}/auth/token/verify/`, data => {
+    apiRequest(localStorage.getItem('token'), `${baseAPIUrl}/auth/verify/`, data => {
         guestLoginField.placeholder = data.username;
     })
 }
@@ -42,20 +46,54 @@ document.getElementById('playClash').addEventListener('click', event => {
 document.getElementById('switchButton').addEventListener('click', event => {
     event.preventDefault();
     const loginButton = document.getElementById("loginButton");
-    if (loginButton.innerText === "Register")
+    if (loginButton.innerText === "Register"){
         loginButton.innerText = "Login";
-    else
+        event.target.innerText = "New? Create an account"
+    }
+    else{
         loginButton.innerText = "Register";
+        event.target.innerText = "Already have an account? log in"
+    }
 })
 
 document.getElementById("loginButton").addEventListener('click', event => {
     event.preventDefault();
     const loginButton = document.getElementById("loginButton");
-    if (loginButton.innerText === "Login")
-        console.log("bite");
-    else {
-        apiRequest(localStorage.getItem(token), `${baseAPIUrl}/auth/register/`, data => {
-            console.log(data);
-        }, "POST")
+    const usernameField = document.getElementById('usernameLogin');
+    const passwordField = document.getElementById('passwordLogin');
+    const userInfo = {
+        'username': usernameField.value,
+        'password': passwordField.value
     }
+    let endpoint;
+    let method;
+    if (loginButton.innerText === "Login") {
+        endpoint = `${baseAPIUrl}/auth/login/`;
+        method = 'POST';
+    }
+    else {
+        endpoint = `${baseAPIUrl}/auth/register/`;
+        method = 'PUT';
+    }
+    apiRequest(localStorage.getItem('token'), endpoint, data => {
+        console.log(data);
+        if (data.access){
+            localStorage.setItem('token', data.access);
+            localStorage.setItem('refresh', data.refresh);
+            document.getElementById('container').innerText = 'sucess';
+        }
+        if (data.username) {
+            document.getElementById('container').innerText = data.username[0];
+            usernameField.style = "background-color:red;"
+        }
+        if (data.password) {
+            document.getElementById('container').innerText = data.username[0];
+            passwordField.style = "background-color:red;"
+        }
+        if (data.detail) {
+            document.getElementById('container').innerText = data.detail;
+        }
+    }, method, undefined, undefined, error=>{
+        document.getElementById('container').innerText = error;
+    }, userInfo)
 })
