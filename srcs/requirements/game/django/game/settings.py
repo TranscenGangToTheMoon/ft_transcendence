3c681aa9e951084b2126dc6fbc601096adf2ec2f
basil <basiljeannot@icloud.com>
2024-10-15 16:44:11 +0200

    finished frontend game

--- srcs/requirements/game/django/game/settings.py
+++ srcs/requirements/game/django/game/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
