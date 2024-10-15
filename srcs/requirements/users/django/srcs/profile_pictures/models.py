bf69007fe0122d671c15764fd0c3d4d49cd49990
fguirama <florian.guiramand@protonmail.com>
2024-10-15 04:43:09 +0200

    remove useless django config files

--- srcs/requirements/users/django/srcs/profile_pictures/models.py
+++ srcs/requirements/users/django/srcs/profile_pictures/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class ProfilePictures(models.Model):
     name = models.CharField(max_length=20, unique=True, editable=False)
     url = models.FilePathField()
