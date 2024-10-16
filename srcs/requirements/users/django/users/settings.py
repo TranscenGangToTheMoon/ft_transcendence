41ac549ac26ef770d050b5cec5b055979b1d8411
limastic <faust.nino@gmail.com>
2024-10-16 20:09:53 +0200

    added cors rules for temp frontend

--- srcs/requirements/users/django/users/settings.py
+++ srcs/requirements/users/django/users/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'block',
+    'corsheaders',
     'friend_requests',
     'friends',
     'profile_pictures',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -58,6 +60,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'users.urls'
+CORS_URLS_REGEX = r"^/api/.*"
+CORS_ALLOWED_ORIGINS = [ # todo useless if in production
+    'http://localhost:8111',
+    'https://localhost:8111',
+]
 
 TEMPLATES = [
     {
