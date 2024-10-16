aa4ebe6883f575818c1d9b7b6559655e766b162e
limastic <faust.nino@gmail.com>
2024-10-16 20:10:46 +0200

    reworked api requests usings promises

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,9 @@
 const baseAPIUrl = "http://localhost:8000/api"
+const baseAPIUrlusers = "http://localhost:8005/api/users"
 let guestLogin = undefined;
 
-function apiRequest(token, endpoint, callback, method="GET", authType="Bearer",
-    contentType="application/json", errorHandling=error=>console.log(error), body=undefined){ 
+function apiRequest(token, endpoint, method="GET", authType="Bearer",
+    contentType="application/json", body=undefined){ 
     let options = {
         method: method,
         headers: {
@@ -14,33 +15,106 @@ function apiRequest(token, endpoint, callback, method="GET", authType="Bearer",
     if (body)
         options.body = JSON.stringify(body);
     console.log(endpoint, options)
-    fetch(endpoint, options)
-        .then(response => response.json())
-        .then(data => callback(data))
-        .catch(error => errorHandling(error))
+    return fetch(endpoint, options)
+        .then(response => {
+            return response.json();
+        })
+        .catch(error =>{
+            throw error;
+        })
 }
 
-document.addEventListener('DOMContentLoaded', event => {
-    apiRequest(undefined, `${baseAPIUrl}/auth/guest/`, data => {
-        localStorage.setItem('token', data.access)
-        localStorage.setItem('refresh', data.refresh)
-    }, "POST")
-    fillGuestPlaceHolder();
+// function getLogin() {
+//     const token = localStorage.getItem('token');
+//     const endpoint = `${baseAPIUrlusers}/me/`;
+//     apiRequest(token, endpoint, data=>{
+//         console.log(data);
+//         return data;
+//     });
+// }
+
+function changePlayFieldValue(login){
+    const playLoginField = document.getElementById("playLoginField");
+    playLoginField.value = login;
+}
+
+async function getDataFromApi(token, endpoint, method="GET", authType="Bearer",
+    contentType="application/json", body=undefined){
+    try {
+        let data = await apiRequest(token, endpoint, method, authType, contentType, body);
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('token'))
+        return;
+    getDataFromApi(undefined, `${baseAPIUrl}/auth/guest/`, "POST")
+        .then(data => {
+            if (data.access) {
+                localStorage.setItem('refresh', data.refresh);
+                localStorage.setItem('token', data.access);
+                fillGuestPlaceHolder();
+            }
+            else
+                console.log('error a gerer', data);
+        })
+        .catch(error => {
+            console.log('error a gerer1', error);
+        })
 })
 
 function fillGuestPlaceHolder(){
-    const guestLoginField = document.getElementById('guestLoginField');
-    apiRequest(localStorage.getItem('token'), `${baseAPIUrl}/auth/verify/`, data => {
-        guestLoginField.placeholder = data.username;
-    })
+    const guestLoginField = document.getElementById('playLoginField');
+    getDataFromApi(localStorage.getItem('token'), `${baseAPIUrlusers}/me/`)
+        .then(data => {
+            if (data.username)
+                guestLoginField.placeholder = data.username;
+            else if (data.detail)
+                console.log('Error: ', data.detail);
+        })
+        .catch(error => {
+            console.log('error a gerer1', error);
+        })
 }
 
-document.getElementById('playDuel').addEventListener('click', event => {
+document.getElementById('playDuel').addEventListener('click', async event => {
     event.preventDefault(); //todo modify guest login 
+    guestUsername = document.getElementById('playLoginField').value;
+    if (guestUsername) {
+        try {
+            let data = await apiRequest(localStorage.getItem('token'), `${baseAPIUrlusers}/me/`, "PATCH",
+            undefined, undefined, {'username' : guestUsername});
+            console.log(data);
+        }
+        catch (error){
+            console.log('error on guest change', error);
+        }
+    }
 })
 
 document.getElementById('playClash').addEventListener('click', event => {
     event.preventDefault(); //todo modify guest login 
+})
+
+document.getElementById('logoutButton').addEventListener('click', event => {
+    getDataFromApi(undefined, `${baseAPIUrl}/auth/guest/`, "POST")
+        .then(data => {
+            if (data.access) {
+                localStorage.setItem('refresh', data.refresh);
+                localStorage.setItem('token', data.access);
+                fillGuestPlaceHolder();
+            }
+            else
+                console.log('error a gerer', data);
+        })
+        .catch(error => {
+            console.log('error a gerer1', error);
+        })
 })
 
 document.getElementById('switchButton').addEventListener('click', event => {
@@ -56,11 +130,23 @@ document.getElementById('switchButton').addEventListener('click', event => {
     }
 })
 
+function loginSuccess() {
+    // getDataFromApi(localStorage.getItem('token'), `${baseAPIUrlusers}/me/`)
+    //     .then(data => {
+    //         var username = data.username;
+    //         document.body.innerHTML = `<h1>page de jeu</h1> (logged as: ${username})`;
+    //     })
+    //     .catch(error => console.log('error a gerer', error));
+    document.getElementById('container').innerText = "bien login mais je peux pas afficher parcce que erreur 500"
+}
+
 document.getElementById("loginButton").addEventListener('click', event => {
     event.preventDefault();
     const loginButton = document.getElementById("loginButton");
     const usernameField = document.getElementById('usernameLogin');
     const passwordField = document.getElementById('passwordLogin');
+    usernameField.style = 'none';
+    passwordField.style = 'none';
     const userInfo = {
         'username': usernameField.value,
         'password': passwordField.value
@@ -75,25 +161,27 @@ document.getElementById("loginButton").addEventListener('click', event => {
         endpoint = `${baseAPIUrl}/auth/register/`;
         method = 'PUT';
     }
-    apiRequest(localStorage.getItem('token'), endpoint, data => {
-        console.log(data);
-        if (data.access){
-            localStorage.setItem('token', data.access);
-            localStorage.setItem('refresh', data.refresh);
-            document.getElementById('container').innerText = 'sucess';
-        }
-        if (data.username) {
-            document.getElementById('container').innerText = data.username[0];
-            usernameField.style = "background-color:red;"
-        }
-        if (data.password) {
-            document.getElementById('container').innerText = data.username[0];
-            passwordField.style = "background-color:red;"
-        }
-        if (data.detail) {
-            document.getElementById('container').innerText = data.detail;
-        }
-    }, method, undefined, undefined, error=>{
-        document.getElementById('container').innerText = error;
-    }, userInfo)
+    getDataFromApi(localStorage.getItem('token'), endpoint, method, undefined, undefined, userInfo)
+        .then(data => {
+            if (data.access){
+                localStorage.setItem('token', data.access);
+                localStorage.setItem('refresh', data.refresh);
+                return loginSuccess();
+            }
+            if (data.username) {
+                document.getElementById('container').innerText = data.username[0];
+                usernameField.style = "background-color:red;"
+            }
+            if (data.password) {
+                console.log(data)
+                document.getElementById('container').innerText = data.password[0];
+                passwordField.style = "background-color:red;"
+            }
+            if (data.detail) {
+                document.getElementById('container').innerText = data.detail;
+            }
+            else
+                console.log('error a gerer', data);
+        })
+        .catch(error => console.log('error a gerer1', error));
 })
