88994a271690b78790c627527f775fed59bee697
basil <basiljeannot@icloud.com>
2024-10-16 18:48:53 +0200

    Revert "websockets working"
    This reverts commit 5e653999a1ff173365b2983987f8e656fdbc3b69.

--- srcs/requirements/game/django/game_server/client_handling.py
+++ srcs/requirements/game/django/game_server/client_handling.py
@@ -1,17 +1,17 @@
 import json
-import uuid
 from channels.generic.websocket import WebsocketConsumer
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
+
 
 class GameClient(WebsocketConsumer):
+	pending_game_request = False
 	def connect(self):
-		self.player_id = str(uuid.uuid4())
 		self.accept()
 		self.send(text_data=json.dumps({
-			'type': 'playerid',
-			'playerid': self.player_id
+			'type': 'connect',
+			'message': 'Connected to the game server.'
 		}))
+		if (pending_game_request):
+
 
 	def disconnect(self, code = None):
 		self.send(text_data=json.dumps({
@@ -19,10 +19,11 @@ class GameClient(WebsocketConsumer):
 			'message': 'Disconnected from the game server.'
 		}))
 
-
-
 	def receive(self, text_data = None, bytes_data = None):
 		if text_data is not None:
 			text_data_json = json.loads(text_data)
-			if (text_data_json['type'] == 'username')
-			user_name = text_data_json['username']
+			message = text_data_json['message']
+			self.send(text_data=json.dumps({
+				'type': 'message',
+				'message': message
+			}))
