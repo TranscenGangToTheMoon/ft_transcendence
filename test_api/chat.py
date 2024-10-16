88a7bc45ae581e49070323358d327f66631a1935
fguirama <florian.guiramand@protonmail.com>
2024-10-16 17:54:27 +0200

    update chat client test API

--- test_api/chat.py
+++ test_api/chat.py
@@ -15,32 +15,19 @@ elif token == 'register':
 elif token == 'paste':
     token = paste()
 else:
-    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI5MDM0MjgxLCJpYXQiOjE3MjkwMzA2ODEsImp0aSI6ImExYTY2YzZmZThiMDQ1ODNiMmNhMTk2ZmU3Yjg1MTU5IiwidXNlcl9pZCI6M30._1w_V6qrZZxXr71Im94p4N1QM_0EUG3-6hkDkKe6VEE'
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI5MDk1MDQ1LCJpYXQiOjE3MjkwOTE0NDUsImp0aSI6IjkwYWQ1M2M0MjkzMTQ3YjZhNjRhODA4NTk3ZWNlNDQxIiwidXNlcl9pZCI6MTV9.abfI1MTn5mgij6ldNZ_KTSQNMb2cLsafAAVkZco76go'
 
+# r = requests.get('http://localhost:8005/api/users/validate/chat/', headers={
+#     'Authorization': 'Bearer ' + token,
+#     'Content-Type': 'application/json'
+# }, data=json.dumps({'username': 'caca'}))
+# print(r.status_code)
+# print(r.json())
 
-caca_token ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI5MDM0NDM2LCJpYXQiOjE3MjkwMzA4MzYsImp0aSI6ImJhZmMzMTUwYTIxNjRkNGM4OGQ3NGY4NDJkNzkxYTQzIiwidXNlcl9pZCI6Mn0.fBTi2HGLpwUezhKXanNC_Xce3FpE1OjJrAJcwthi844'
-hey_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI5MDM0MjgxLCJpYXQiOjE3MjkwMzA2ODEsImp0aSI6ImExYTY2YzZmZThiMDQ1ODNiMmNhMTk2ZmU3Yjg1MTU5IiwidXNlcl9pZCI6M30._1w_V6qrZZxXr71Im94p4N1QM_0EUG3-6hkDkKe6VEE'
-
-
-r = requests.post('http://localhost:8005/api/users/me/friend_requests/', headers={
-    'Authorization': 'Bearer ' + hey_token,
-    'Content-Type': 'application/json'
-}, data=json.dumps({'username': 'caca'}))
-
-print(r.status_code)
-print(r.json())
-
-r = requests.post('http://localhost:8005/api/users/me/friends/', headers={
-    'Authorization': 'Bearer ' + caca_token,
-    'Content-Type': 'application/json'
-}, data=json.dumps({'username': 'hey'}))
-
-print(r.json())
-
-r = requests.get('http://localhost:8005/api/users/validate/chat/', headers={
+r = requests.get('http://localhost:8002/api/chat/10/messages/', headers={
     'Authorization': 'Bearer ' + token,
     'Content-Type': 'application/json'
-}, data=json.dumps({'username': 'caca'}))
+}, data=json.dumps({'username': 'caca', 'type': 'private_message', 'content': 'first'}))
 
 print(r.status_code)
 if r.status_code == 204 or r.status_code == 500:
