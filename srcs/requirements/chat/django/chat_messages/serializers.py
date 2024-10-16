a114e07f2e1a226aaa82f110c03941d5425edd42
fguirama <florian.guiramand@protonmail.com>
2024-10-16 17:56:25 +0200

    add chat messages

---
+++ srcs/requirements/chat/django/chat_messages/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from chat_messages.models import Messages
+from chats.models import Chats, ChatParticipants
+
+
+class MessagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Messages
+        fields = ('id', 'chat_id', 'author', 'content', 'sent_at')
+        read_only_fields = ('id', 'author', 'chat_id')
+
+    def create(self, validated_data):
+        pk = self.context.get('pk')
+        if pk is None:
+            raise serializers.ValidationError({'error': 'Chat id is required'})
+        if not Chats.objects.filter(pk=pk).exists():
+            raise serializers.ValidationError({'chat_id': 'Chat does not exist.'})
+        user = self.context['auth_user']
+        if not ChatParticipants.objects.filter(chat_id=pk, user_id=user['id']).exists():
+            raise serializers.ValidationError({'chat_id': 'You are not participant of this chat'})
+
+        validated_data['chat_id'] = pk
+        validated_data['author'] = user['id']
+        return super().create(validated_data)
