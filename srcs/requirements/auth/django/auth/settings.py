280665e9ec0201f4d63f14e30bcc94908c1cc3c1
fguirama <florian.guiramand@protonmail.com>
2024-10-19 18:47:26 +0200

    fix auth refresh token

--- srcs/requirements/auth/django/auth/settings.py
+++ srcs/requirements/auth/django/auth/settings.py
@@ -153,5 +153,8 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ['Bearer'],
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': True,
 }
