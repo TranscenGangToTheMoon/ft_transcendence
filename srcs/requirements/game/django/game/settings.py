a88d6c2730be26a5b23d9a9c06b5daf5488566fc
fguirama <florian.guiramand@protonmail.com>
2024-10-26 19:25:07 +0200

    configure postre db in game service

--- srcs/requirements/game/django/game/settings.py
+++ srcs/requirements/game/django/game/settings.py
@@ -85,8 +85,12 @@ ASGI_APPLICATION = 'game.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': environ["POSTGRES_DB"],
+        'USER': environ["POSTGRES_USER"],
+        'PASSWORD': environ["POSTGRES_PASSWORD"],
+        'HOST': environ["POSTGRES_HOST"],
+        'PORT': '5432',
     }
 }
 
