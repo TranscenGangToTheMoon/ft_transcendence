c0050d90fd9002072e0aab5d629edc672aecd29a
fguirama <florian.guiramand@protonmail.com>
2024-10-26 18:37:49 +0200

    change matchmaking endpoint => api/play/*

--- srcs/requirements/matchmaking/django/matchmaking/urls.py
+++ srcs/requirements/matchmaking/django/matchmaking/urls.py
@@ -21,11 +21,11 @@ from tournament.views import tournament_view, tournament_participants_view, tour
     tournament_result_match_view
 
 urlpatterns = [
-    path('api/lobby/', lobby_create_update_view),
-    path('api/lobby/<str:code>/', lobby_create_list_delete_view),
+    path('api/play/lobby/', lobby_create_update_view),
+    path('api/play/lobby/<str:code>/', lobby_create_list_delete_view),
 
-    path('api/tournament/', tournament_view),
+    path('api/play/tournament/', tournament_view),
+    path('api/play/tournament/<str:code>/', tournament_participants_view),
+    path('api/play/tournament/<str:code>/kick/<int:user_id>/', tournament_kick_view),
     path('api/tournament/result-match/', tournament_result_match_view),
-    path('api/tournament/<str:code>/', tournament_participants_view),
-    path('api/tournament/<str:code>/kick/<int:user_id>/', tournament_kick_view),
 ]
