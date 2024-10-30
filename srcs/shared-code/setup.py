cd4ea89f9e9cae861763a8bc7f76293aa3e32895
fguirama <florian.guiramand@protonmail.com>
2024-10-30 19:55:14 +0100

    rename transcendence-lib in lib-transcendence

--- srcs/shared-code/setup.py
+++ srcs/shared-code/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="transcendence-lib",
+    name="lib-transcendence",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
