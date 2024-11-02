03158f62a13e49fb5827d07be179bf07f7c52cc5
fguirama <florian.guiramand@protonmail.com>
2024-11-02 03:31:25 +0100

    add block view in chat

--- srcs/requirements/chat/django/users/views.py
+++ srcs/requirements/chat/django/users/views.py
@@ -1,13 +1,17 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, serializers
 from rest_framework.response import Response
 
 from chats.models import ChatParticipants, Chats
+from chats.serializers import BlockChatSerializer
+from chats.utils import get_chat_together
 
 
 #todo try to make with generics view
 class RenameUserView(generics.UpdateAPIView):
     def update(self, request, *args, **kwargs):
         new_username = request.data.get('username')
+        if not new_username:
+            raise serializers.ValidationError({'username': ['This field is required.']})
         players_queryset = ChatParticipants.objects.filter(user_id=kwargs['user_id'])
         updated_count = players_queryset.update(username=new_username)
 
@@ -15,6 +19,13 @@ class RenameUserView(generics.UpdateAPIView):
             {'message': f'{updated_count} players updated successfully'},
             status=status.HTTP_200_OK
         )
+
+
+class UpdateBlockUserView(generics.UpdateAPIView):
+    serializers = BlockChatSerializer
+
+    def get_object(self): #todo test si ca marche
+        return get_chat_together(self.kwargs['user_id'], self.kwargs['user_block'])
 
 
 class DeleteUserView(generics.DestroyAPIView):
@@ -25,4 +36,5 @@ class DeleteUserView(generics.DestroyAPIView):
 
 
 rename_user_view = RenameUserView.as_view()
+block_user_view = UpdateBlockUserView.as_view()
 delete_user_view = DeleteUserView.as_view()
