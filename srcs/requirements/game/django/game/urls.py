9d7376cf588eefecb5365c607d20724791412a23
basil <basiljeannot@icloud.com>
2024-11-02 03:20:49 +0100

    begin support for socketio in game server

--- srcs/requirements/game/django/game/urls.py
+++ srcs/requirements/game/django/game/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('game/', game_server),
     path('api/match/', match_create_view),
     path('api/tournaments/', save_tournament_view),
-
     path('api/game/match/<int:user_id>/', match_list_view),
     path('api/game/tournaments/<int:pk>/', retrieve_tournament_view),
 ]
