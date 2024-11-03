ce9ac191fe04b1650992fba0a603291d5960ceeb
fguirama <florian.guiramand@protonmail.com>
2024-11-03 11:59:25 +0100

    update request_service for raison correct error

--- srcs/shared-code/lib_transcendence/request.py
+++ srcs/shared-code/lib_transcendence/request.py
@@ -2,7 +2,7 @@ import json
 from typing import Literal
 
 from rest_framework import status
-from rest_framework.exceptions import APIException, AuthenticationFailed
+from rest_framework.exceptions import APIException, AuthenticationFailed, PermissionDenied, MethodNotAllowed
 import requests
 
 
@@ -24,7 +24,7 @@ def request_service(service: Literal['auth', 'chat', 'game', 'matchmaking', 'use
         headers['Authorization'] = authorization
 
     try:
-        print(method, f'{service}/api/{endpoint}', flush=True)
+        print(method, f'=> {service}/api/{endpoint}', flush=True)
         response = requests.request(
             method=method,
             url=f'http://{service}:8000/api/{endpoint}',
@@ -37,8 +37,16 @@ def request_service(service: Literal['auth', 'chat', 'game', 'matchmaking', 'use
 
         json_data = response.json()
         print(json_data, flush=True)
-        if response.status_code not in (200, 201, 202, 203, 205, 206):
+        if response.status_code == 401:
             raise AuthenticationFailed(json_data)
+        if response.status_code == 400:
+            raise PermissionDenied(json_data)
+        if response.status_code == 403:
+            raise PermissionDenied(json_data)
+        if response.status_code == 405:
+            raise MethodNotAllowed(method)
+        if response.status_code == 503:
+            raise ServiceUnavailable(service)
     except (requests.ConnectionError, requests.exceptions.JSONDecodeError):
         raise ServiceUnavailable(service)
     return json_data
