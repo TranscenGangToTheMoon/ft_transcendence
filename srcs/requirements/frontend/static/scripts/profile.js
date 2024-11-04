a04ba89cd0a23306d3442f04ec4d9353c4266238
limastic <faust.nino@gmail.com>
2024-11-04 19:07:28 +0100

    finished Account part (for now; to update when API Error is solved)

--- srcs/requirements/frontend/static/scripts/profile.js
+++ srcs/requirements/frontend/static/scripts/profile.js
@@ -1,4 +1,17 @@
+async function loadTabs() {
+    const tabFiles = {
+        'account' : 'profileTemplates/account.html',
+        'friends' : 'profileTemplates/friends.html',
+    }
+
+    for (const key in tabFiles){
+        loadContent(tabFiles[key], key);
+        console.log(`je load ${tabFiles[key]} dans la div ${key}`);
+    }
+}
+
 async function atStart() {
+    loadTabs();
     await fetchUserInfos(true);
     await loadUserProfile();
 }
