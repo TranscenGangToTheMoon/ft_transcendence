e58f66ce79dee89a793235c4da059e4705d70d3f
fguirama <florian.guiramand@protonmail.com>
2024-11-05 13:57:55 +0100

    remake get_tournament, get_lobby, get_lobby_participant, get_tournament_participant
    - Use same function for both
    - Make better error message
    - Move both function to matchmaking/utils.py
    - Use get_lobby in LobbyParticipantsSerializer.create
    - Make get_kick_participants

--- srcs/requirements/matchmaking/django/matchmaking/utils.py
+++ srcs/requirements/matchmaking/django/matchmaking/utils.py
@@ -9,7 +9,8 @@ from play.models import Players
 from tournament.models import Tournaments, TournamentParticipants
 
 
-def get_participants(name: Literal['lobby', 'tournament'], model, obj, user_id, creator_check):
+# -------------------- GET PARTICIPANT ------------------------------------------------------------------------------- #
+def get_participant(name: Literal['lobby', 'tournament'], model, obj, user_id, creator_check, from_place):
     kwargs = {'user_id': user_id}
     if obj is not None:
         kwargs[name] = obj.id
@@ -20,11 +21,66 @@ def get_participants(name: Literal['lobby', 'tournament'], model, obj, user_id, 
             raise PermissionDenied(f'You are not creator of this {name}.')
         return p
     except model.DoesNotExist:
-        if obj is None:
+        if from_place:
             raise NotFound(f'You are not in any {name}.')
-        raise NotFound(f'You are not a participant of this {name}.')
+        raise NotFound(f'You do not belong to this {name}.')
 
 
+def get_lobby_participant(lobby, user_id, creator_check=False, from_place=False):
+    return get_participant('lobby', LobbyParticipants, lobby, user_id, creator_check, from_place)
+
+
+def get_tournament_participant(tournament, user_id, creator_check=False, from_place=False):
+    return get_participant('tournament', TournamentParticipants, tournament, user_id, creator_check, from_place)
+
+
+def get_kick_participants(name: Literal['lobby', 'tournament'], model, user_id):
+    try:
+        return model.participants.get(user_id=user_id)
+    except LobbyParticipants.DoesNotExist:
+        raise NotFound(f'This user does not belong to this {name}.')
+
+
+# -------------------- GET PLACE ------------------------------------------------------------------------------------- #
+def get_place(name: Literal['lobby', 'tournament'], model, create, **kwargs):
+    allowed_keys = ('id', 'code')
+
+    key = list(kwargs)[0]
+    assert len(kwargs) == 1 and key in allowed_keys
+
+    value = kwargs[key]
+    if value is None:
+        raise serializers.ValidationError({key: [f'{name.title()} {key} is required.']})
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        if create:
+            raise NotFound({'code': [f'{name.title()} code does not exist.']})
+        raise NotFound(f'You do not belong to this {name}.')
+
+
+def get_lobby(code, create=False):
+    return get_place('lobby', Lobby, create, code=code)
+
+
+def get_tournament(create=False, **kwargs):
+    return get_place('tournament', Tournaments, create, **kwargs)
+
+
+# -------------------- GET PARTICIPANT ------------------------------------------------------------------------------- #
+# todo move to library
+def create_match(tournament_id, stage_id, teams):
+    data = {
+        'tournament_id': tournament_id,
+        'tournament_stage_id': stage_id,
+        'game_mode': 'tournament',
+        'teams': teams
+    }
+
+    requests_game('match/', data=data)
+
+
+# -------------------- VERIFY USER ----------------------------------------------------------------------------------- #
 def verify_user(user_id, join_tournament=True):
     try:
         participant = TournamentParticipants.objects.get(user_id=user_id, still_in=True)
@@ -53,6 +109,7 @@ def verify_user(user_id, join_tournament=True):
         pass
 
 
+# -------------------- BLOCK CHECK ----------------------------------------------------------------------------------- #
 def can_join(request, obj, new_user):
     try:
         self_user = obj.participants.get(creator=True).user_id
