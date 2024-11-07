b0abc9cc171e8a45f2ea002ac13c3761c3f91ce0
fguirama <florian.guiramand@protonmail.com>
2024-11-07 12:59:11 +0100

    remove AllowedHostPermission

--- srcs/shared-code/lib_transcendence/auth.py
+++ srcs/shared-code/lib_transcendence/auth.py
@@ -15,18 +15,6 @@ class IsAuthenticated(permissions.BasePermission):
         return True
 
 
-class AllowedHostPermission(permissions.BasePermission):
-    ALLOWED_HOSTS = ['game']
-
-    def has_permission(self, request, view):
-        host = request.get_host().split(':')[0]
-        if host not in self.ALLOWED_HOSTS:
-            raise PermissionDenied(
-                detail="Authentication failed: Request must come from an authorized host."
-            )
-        return True
-
-
 def get_auth_user(request=None):
     if request is None:
         raise serializers.ValidationError(MessagesException.ValidationError.REQUEST_REQUIRED)
