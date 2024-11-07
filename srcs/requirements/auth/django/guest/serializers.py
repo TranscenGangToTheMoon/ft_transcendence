b0e4826e13516796425e8a4053153526649b0789
fguirama <florian.guiramand@protonmail.com>
2024-11-07 12:02:41 +0100

    remove useless import in auth/guest/serializers.py

--- srcs/requirements/auth/django/guest/serializers.py
+++ srcs/requirements/auth/django/guest/serializers.py
@@ -1,6 +1,3 @@
-from random import choices
-from string import digits
-
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
