4bf4765ab14de3d95f4870b1f1dd9a805d6bba7a
xcharra <xcharra@student.42lyon.fr>
2024-11-08 15:28:13 +0100

    Prepare future conditionnal migration (prod or not)
    Add env var MIGRATION to set at true or false
    Rename prod compose file

--- srcs/requirements/users/init.d/entrypoint.sh
+++ srcs/requirements/users/init.d/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+RED="\001\033[031m\002"
+BOLD="\001\033[001m\002"
+RESET="\001\033[000m\002"
 
-python manage.py makemigrations
+echo -e $BOLD$RED"- Users migrations processing"$RESET
+
+if [[ $MIGRATION = true ]]; then
+    echo -e $BOLD$RED"- Making migrations"$RESET
+    python manage.py makemigrations
+fi
+echo -e $BOLD$RED"- Migrating"$RESET
 python manage.py migrate
 
 exec "$@"
