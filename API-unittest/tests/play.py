7ac384722c186d42e1f0717d33e77e4416e97261
fguirama <florian.guiramand@protonmail.com>
2024-11-10 17:54:20 +0100

    make play unit tests

--- API-unittest/tests/play.py
+++ API-unittest/tests/play.py
@@ -1,5 +1,7 @@
+from services.game import create_game
 from services.lobby import create_lobby, join_lobby
 from services.play import play
+from services.tournament import join_tournament, create_tournament
 from utils.credentials import new_user, guest_user
 from utils.my_unittest import UnitTest
 
@@ -9,19 +11,29 @@ class Test01_Play(UnitTest):
     def test_001_play_duel(self):
         self.assertResponse(play(), 201)
 
-    def test_001_play_ranked(self):
+    def test_002_play_ranked(self):
         self.assertResponse(play(game_mode='ranked'), 201)
 
 
 class Test02_PlayError(UnitTest):
 
     def test_001_already_in_game(self):
-        self.assertResponse(play(), 201)
-        self.assertResponse(play(), 201) # todo ??
+        user1 = new_user()
+
+        self.assertResponse(create_game(user1, new_user()), 201)
+        self.assertResponse(play(user1), 409, {'detail': 'You are already in a game.'})
 
     def test_002_already_in_tournament(self):
-        # self.assertResponse(create_tournament(), create_tournament201)
-        self.assertResponse(play(), 201)
+        user1 = new_user()
+
+        response = create_tournament(user1)
+        self.assertResponse(response, 201)
+        code = response.json['code']
+
+        for i in range(3):
+            self.assertResponse(join_tournament(code), 201)
+
+        self.assertResponse(play(user1), 409, {'detail': 'You are already in a tournament.'})
 
     def test_003_guest_cannot_play_ranked(self):
         self.assertResponse(play(guest_user(), 'ranked'), 403, {'detail': 'Guest users cannot play ranked games.'})
@@ -29,24 +41,33 @@ class Test02_PlayError(UnitTest):
     def test_004_user_in_lobby(self):
         user1 = new_user()
 
-        self.assertResponse(create_lobby(user1), 201)
-        self.assertResponse(play(user1), 201)
-
-        response = create_lobby(user1, method='GET')
-        self.assertResponse(response, 200, {'detail': 'You do not belong to any lobby.'})
+        response = create_lobby(user1)
+        self.assertResponse(response, 201)
         code = response.json['code']
 
-        self.assertResponse(join_lobby(code, user1, 'GET'), 404, {'detail': 'Lobby not found.'})
+        self.assertResponse(play(user1), 201)
+        self.assertResponse(create_lobby(user1, method='GET'), 404, {'detail': 'You do not belong to any lobby.'})
+        self.assertResponse(join_lobby(code, user1, 'GET'), 403, {'detail': 'You do not belong to this lobby.'})
 
-    # def test_004_user_in_tournament(self): todo
-    #     user1 = new_user()
-    #
-    #     self.assertResponse(create_lobby(user1), 200)
-    #     self.assertResponse(play(user1), 201)
-    #     response = create_lobby(user1, method='GET')
-    #     self.assertResponse(response, 200, {'detail': 'You are not in any lobby.'})
-    #     code = response.json['code']
-    #     self.assertResponse(join_lobby(code, user1, 'GET'), 404, {'detail': 'Lobby not found.'})
+    def test_005_user_in_tournament(self):
+        user1 = new_user()
+
+        response = create_tournament(user1)
+        self.assertResponse(response, 201)
+        code = response.json['code']
+
+        self.assertResponse(play(user1), 201)
+        self.assertResponse(create_tournament(user1, method='GET'), 404, {'detail': 'You do not belong to any tournament.'})
+        self.assertResponse(join_tournament(code, user1, 'GET'), 403, {'detail': 'You do not belong to this tournament.'})
+
+    def test_006_delete(self):
+        user1 = new_user()
+
+        self.assertResponse(play(user1), 201)
+        self.assertResponse(play(user1, method='DELETE'), 204)
+
+    def test_006_delete_not_play(self):
+        self.assertResponse(play(method='DELETE'), 404, {'detail': 'You are not currently playing.'})
 
 
 if __name__ == '__main__':
