9675e16a71c75a99f6ad873ac6fbec551ec91722
fguirama <florian.guiramand@protonmail.com>
2024-11-10 17:33:52 +0100

    add method params in play() service

--- API-unittest/services/play.py
+++ API-unittest/services/play.py
@@ -4,11 +4,11 @@ from utils.credentials import new_user
 from utils.request import make_request
 
 
-def play(user=None, game_mode: Literal['ranked', 'duel'] = 'duel'):
+def play(user=None, game_mode: Literal['ranked', 'duel'] = 'duel', method: Literal['POST', 'DELETE'] = 'POST'):
     if user is None:
         user = new_user()
     return make_request(
         endpoint=f'play/{game_mode}/',
-        method='POST',
+        method=method,
         token=user['token'],
     )
