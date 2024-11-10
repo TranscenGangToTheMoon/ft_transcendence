e378a5528cdbf9c77a8c630170d7f46d37f5dee6
fguirama <florian.guiramand@protonmail.com>
2024-11-10 18:05:57 +0100

    rename block -> blocked

--- srcs/requirements/matchmaking/django/matchmaking/urls.py
+++ srcs/requirements/matchmaking/django/matchmaking/urls.py
@@ -5,7 +5,7 @@ from lobby.views import lobby_view, lobby_participants_view, lobby_kick_view
 from play.views import duel_view, ranked_view
 from tournament.views import tournament_view, tournament_search_view, tournament_participants_view, tournament_kick_view, \
     tournament_result_match_view
-from users.views import block_user_view, delete_user_view
+from users.views import blocked_user_view, delete_user_view
 
 urlpatterns = [
     path(Matchmaking.duel, duel_view),
@@ -22,6 +22,6 @@ urlpatterns = [
 
     path(Matchmaking.tournament_result_match, tournament_result_match_view),
 
-    path(UsersManagement.blocked_user, block_user_view),
+    path(UsersManagement.blocked_user, blocked_user_view),
     path(UsersManagement.delete_user, delete_user_view),
 ]
