b93c0377118632194eda4e7a8d707a0c02884a1c
fguirama <florian.guiramand@protonmail.com>
2024-11-10 17:52:35 +0100

    rename block -> blocked

--- srcs/requirements/chat/django/chat/urls.py
+++ srcs/requirements/chat/django/chat/urls.py
@@ -3,7 +3,7 @@ from lib_transcendence.endpoints import Chat, UsersManagement
 
 from chat_messages.views import messages_view
 from chats.views import chats_view, chat_view
-from users.views import rename_user_view, block_user_view, delete_user_view
+from users.views import rename_user_view, blocked_user_view, delete_user_view
 
 urlpatterns = [
     path(Chat.chats, chats_view),
@@ -11,6 +11,6 @@ urlpatterns = [
     path(Chat.messages, messages_view),
 
     path(UsersManagement.rename_user, rename_user_view),
-    path(UsersManagement.blocked_user, block_user_view),
+    path(UsersManagement.blocked_user, blocked_user_view),
     path(UsersManagement.delete_user, delete_user_view),
 ]
