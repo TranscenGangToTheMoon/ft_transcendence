61eaab6944024b34710474fc76fc6dce35775ed9
fguirama <florian.guiramand@protonmail.com>
2024-11-10 17:47:19 +0100

    add -e when install lib (for dev)

--- srcs/requirements/game/entrypoint.sh
+++ srcs/requirements/game/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sleep 20
-python -m pip install /shared/ # todo remove in prod
+python -m pip install -e /shared/ # todo remove in prod
 python manage.py makemigrations
 python manage.py migrate
 
