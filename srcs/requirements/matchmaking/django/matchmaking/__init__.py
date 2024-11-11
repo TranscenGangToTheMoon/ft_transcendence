09f714f283eac05fa01f871b8d5b6afda83a1d37
basil <basiljeannot@icloud.com>
2024-11-11 13:48:33 +0100

    matchmaking service is operational
    matchmaking working fine at project launch for ranked and duel playing requests

--- srcs/requirements/matchmaking/django/matchmaking/__init__.py
+++ srcs/requirements/matchmaking/django/matchmaking/__init__.py
@@ -1,1 +1,10 @@
 from .lookup import RangeLookup
+from threading import Thread
+import time
+
+def wait_then_launch():
+	time.sleep(10)
+	from .matchmaking import make_match
+	make_match()
+
+thread = Thread(target=wait_then_launch).start()
--- srcs/requirements/matchmaking/django/matchmaking/matchmaking.py
+++ srcs/requirements/matchmaking/django/matchmaking/matchmaking.py
@@ -1,24 +1,19 @@
-import sys
-import os
-
+from datetime import datetime, timezone, timedelta
 from django.http import response
-from rest_framework import status
-# sys.path.append(os.path.abspath('./'))
-# print(os.listdir('./'))
-# print(sys.path.__str__())
-import datetime
 from lib_transcendence.GameMode import GameMode
 from lib_transcendence.request import ParseError
 from lib_transcendence.services import requests_game
-from rest_framework.exceptions import APIException
-from datetime import datetime, timezone, timedelta
-from play.models import Players
-import time
-from threading import Thread
 from math import sqrt
+from play.models import Players
+from rest_framework import status
+from rest_framework.exceptions import APIException
+from threading import Thread
+from time import sleep
+import os
+import sys
 
 def post_match(player1, player2, game_mode):
-	requests_game('match/', 'POST', {'game_mode': game_mode, 'teams': [[player1.user_id], [player2.user_id]]}) #todo check for status code
+	requests_game('match/', 'POST', {'game_mode': game_mode, 'teams': [[player1.user_id], [player2.user_id]]}) #TODO -> check for status code
 	print('made request for: ', player1.user_id, ' ', player1.trophies, ' ', player2.user_id, ' ', player2.trophies, flush=True)
 	player1.delete()
 	player2.delete()
@@ -62,16 +57,16 @@ def make_match():
 				post_match(duel_players[0], duel_players[1], GameMode.duel)
 			except(APIException):
 				pass
-				# todo inform client
+				#TODO -> inform client
 		ranked_players = players.filter(game_mode=GameMode.ranked)
 		find_ranked_match(ranked_players)
-					# todo -> inform client
-					# todo -> blocked user can't play together
+					#TODO -> inform client
+					#TODO -> blocked user can't play together
 	return "finished matchmaking"
 
 def launch_matchmaking(request):
-	# while (True):
-	# 	time.sleep(5)
-	# 	make_match()
-	thread = Thread(target=make_match).start()
+	sleep(20)
+	while (True):
+		sleep(1)
+		make_match()
 	return response.HttpResponse('<h1>Matchmaking started<h1/>', status=status.HTTP_200_OK)
