1accf406a2d17cd2137758e6d4a449b14affe699
fguirama <florian.guiramand@protonmail.com>
2024-11-11 13:01:34 +0100

    add install lib_transcendence in all entrypoint.sh

--- srcs/requirements/chat/init.d/entrypoint.sh
+++ srcs/requirements/chat/init.d/entrypoint.sh
@@ -3,6 +3,9 @@ RED="\001\033[031m\002"
 BOLD="\001\033[001m\002"
 RESET="\001\033[000m\002"
 
+sleep 5
+pip install -e /shared/ # todo remove in prod
+
 echo -e $BOLD$RED"- Chat migrations processing"$RESET
 
 if [[ $MIGRATION = true ]]; then
