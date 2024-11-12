6f9766ea0764fc5a4b409f999478e49067674e07
Basile Jeannot <bajeanno@student.42.fr>
2024-11-12 16:26:53 +0100

    changed socketio server listening port (8000 -> 5000)

--- srcs/requirements/game/django/game/socket_server.py
+++ srcs/requirements/game/django/game/socket_server.py
@@ -33,4 +33,4 @@ async def disconnect(sid):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(application, host='0.0.0.0', port=8000)
+    uvicorn.run(application, host='0.0.0.0', port=5000)
