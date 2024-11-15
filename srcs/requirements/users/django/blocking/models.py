3da26e71b2bc3581fa7bb85e6de81ee098d49617
fguirama <florian.guiramand@protonmail.com>
2024-11-16 00:27:49 +0100

    add method blocked_services() in BlockedUsers models
    - send request to chat service to inform that userX blocked userY. Then chat service make chat between two user inaccessible
    - send request to matchmaking service to inform that userX blocked userY. Then matchmaking service kick blocked user if there are in the same lobby or tournament
    - when status='unblock', send request to chat service to inform that userX unblocked userY. Then chat service make chat between two user accessible again

--- srcs/requirements/users/django/blocking/models.py
+++ srcs/requirements/users/django/blocking/models.py
@@ -1,4 +1,9 @@
+from typing import Literal
+
 from django.db import models
+from lib_transcendence import endpoints
+from lib_transcendence.services import request_chat, request_matchmaking
+from rest_framework.exceptions import APIException
 
 from users.models import Users
 
@@ -10,3 +15,23 @@ class BlockedUsers(models.Model):
 
     def __str__(self):
         return f'{self.id}/ {self.user} => {self.blocked}'
+
+    def blocked_services(self, status: Literal['block', 'unblock'] = 'block'):
+        endpoint = endpoints.UsersManagement.fblocked_user.format(user_id=self.user_id, blocked_user_id=self.blocked_id)
+
+        try:
+            request_chat(
+                endpoint=endpoint,
+                data={'blocked': status == 'block'},
+            )
+        except APIException:
+            pass
+
+        if status == 'block':
+            try:
+                request_matchmaking(
+                    endpoint=endpoint,
+                    method='DELETE',
+                )
+            except APIException:
+                pass
