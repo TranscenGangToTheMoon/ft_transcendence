437128e5cba8cea0bd99cd776d03ad68dd7c46f6
fguirama <florian.guiramand@protonmail.com>
2024-11-16 00:30:43 +0100

    surcharge perform_destroy() method for add calling of blocked_services(status='unblock') method

--- srcs/requirements/users/django/blocking/views.py
+++ srcs/requirements/users/django/blocking/views.py
@@ -20,6 +20,10 @@ class BlockedDeleteView(generics.DestroyAPIView, BlockedMixin):
         except BlockedUsers.DoesNotExist:
             raise PermissionDenied(MessagesException.PermissionDenied.NOT_BELONG_BLOCKED)
 
+    def perform_destroy(self, instance):
+        instance.blocked_services('unblock')
+        return super().perform_destroy(instance)
+
 
 blocked_list_create_view = BlockedListCreateView.as_view()
 blocked_delete_view = BlockedDeleteView.as_view()
