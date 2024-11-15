0832be80e2e0acf1bbaf4d646d3e20aa76aa254b
fguirama <florian.guiramand@protonmail.com>
2024-11-16 00:23:01 +0100

    inherit SerializerContext since surcharge get_serializer_context()

--- srcs/requirements/chat/django/chat_messages/views.py
+++ srcs/requirements/chat/django/chat_messages/views.py
@@ -1,28 +1,19 @@
 from rest_framework import generics
-from lib_transcendence.exceptions import MessagesException
+from lib_transcendence.serializer import SerializerContext
 
 from chat_messages.models import Messages
 from chat_messages.serializers import MessagesSerializer
 from chats.models import ChatParticipants
 
 
-class MessagesView(generics.ListCreateAPIView):
+class MessagesView(SerializerContext, generics.ListCreateAPIView):
     queryset = Messages.objects.all().order_by('-sent_at')
     serializer_class = MessagesSerializer
     lookup_field = 'pk'
 
     def filter_queryset(self, queryset):
-        try:
-            ChatParticipants.objects.get(chat_id=self.kwargs['pk'], user_id=self.request.user.id)
-            return queryset.filter(chat_id=self.kwargs['pk'])
-        except ChatParticipants.DoesNotExist:
-            raise PermissionDenied(MessagesException.PermissionDenied.NOT_BELONG_TO_CHAT)
+        chat_id = self.kwargs['chat_id']
 
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context['pk'] = self.kwargs['pk']
-        context['auth_user'] = self.request.data['auth_user']
-        return context
 
 
 messages_view = MessagesView.as_view()
