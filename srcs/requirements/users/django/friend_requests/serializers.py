ba0b32d3d476ac01111c8f8fd7ef738699dd0470
fguirama <florian.guiramand@protonmail.com>
2024-11-16 18:17:45 +0100

    fix verification order in FriendRequestsSerializer.create()

--- srcs/requirements/users/django/friend_requests/serializers.py
+++ srcs/requirements/users/django/friend_requests/serializers.py
@@ -24,25 +24,27 @@ class FriendRequestsSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         sender = get_user(self.context.get('request'))
-
-        if sender.sent_friend_requests.count() > 20:
-            raise PermissionDenied(MessagesException.PermissionDenied.SEND_MORE_THAN_20_FRIEND_REQUESTS)
-
         receiver = get_valid_user(sender, validated_data.pop('username'))
 
         if receiver == sender:
             raise PermissionDenied(MessagesException.PermissionDenied.SEND_FRIEND_REQUEST_YOURSELF)
 
-        if sender.blocked.filter(blocked=receiver).exists():
-            raise PermissionDenied(MessagesException.PermissionDenied.BLOCKED_USER)
-
         if is_friendship(sender, receiver):
             raise ResourceExists(MessagesException.ResourceExists.FRIEND)
 
+        if sender.friend_requests_sent.filter(receiver=receiver).exists():
+            raise ResourceExists(MessagesException.ResourceExists.FRIEND_REQUEST_SENT)
+
+        if receiver.friend_requests_sent.filter(receiver=sender).exists():
+            raise ResourceExists(MessagesException.ResourceExists.FRIEND_REQUEST_RECEIVED)
+
+        if sender.blocked.filter(blocked=receiver).exists():
+            raise PermissionDenied(MessagesException.PermissionDenied.BLOCKED_USER)
+
+        if sender.friend_requests_sent.count() > 20:
+            raise PermissionDenied(MessagesException.PermissionDenied.SEND_MORE_THAN_20_FRIEND_REQUESTS)
+
         if not receiver.accept_friend_request:
             raise PermissionDenied(MessagesException.PermissionDenied.NOT_ACCEPT_FRIEND_REQUEST)
-
-        if sender.sent_friend_requests.filter(receiver=receiver).exists():
-            raise ResourceExists(MessagesException.ResourceExists.FRIEND_REQUEST)
 
         return super().create({'sender': sender, 'receiver': receiver})
