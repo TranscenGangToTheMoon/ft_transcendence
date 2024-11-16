74735da7eb7ae87f60dce8d537efd3eaae60e0df
fguirama <florian.guiramand@protonmail.com>
2024-11-16 20:37:23 +0100

    make all test_friends

--- API-unittest/tests/test_friends.py
+++ API-unittest/tests/test_friends.py
@@ -3,7 +3,7 @@ from services.friend import create_friend, accept_friend_request, send_friend_re
 from utils.credentials import new_user
 from utils.my_unittest import UnitTest
 
-# 1. todo make test
+
 class Test01_Friend(UnitTest):
 
     def test_001_friend(self):
