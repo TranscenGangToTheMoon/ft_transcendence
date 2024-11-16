956b39c7d0b4a43f6a357cc94c4ae8707a9a6613
fguirama <florian.guiramand@protonmail.com>
2024-11-16 20:38:05 +0100

    make all test_friends

--- API-unittest/tests/test_friends.py
+++ API-unittest/tests/test_friends.py
@@ -1,5 +1,6 @@
 from services.blocked import blocked_user
-from services.friend import create_friend, accept_friend_request, send_friend_request, receive_friend_requests
+from services.friend import create_friendship, friend_request, friend_requests, get_friend_requests_received, \
+    get_friends, friend
 from utils.credentials import new_user
 from utils.my_unittest import UnitTest
 
@@ -7,7 +8,7 @@ from utils.my_unittest import UnitTest
 class Test01_Friend(UnitTest):
 
     def test_001_friend(self):
-        self.assertFriendResponse(create_friend())
+        self.assertFriendResponse(create_friendship())
 
     def test_002_friend_without_friend_request(self):
         self.assertResponse(accept_friend_request(), 404, {'detail': 'Friend request not found.'})
@@ -26,16 +27,16 @@ class Test01_Friend(UnitTest):
         user1 = new_user()
 
         for i in range(7):
-            self.assertFriendResponse(create_friend(user1))
+            self.assertFriendResponse(create_friendship(user1))
 
-        self.assertResponse(accept_friend_request(user1, method='GET'), 200, count=7)
+        self.assertResponse(get_friends(user1), 200, count=7)
 
-    def test_006_friends_then_block(self):
+    def test_004_friends_then_block(self):
         user1 = new_user()
         user2 = new_user()
 
-        self.assertFriendResponse(create_friend(user1, user2))
-        self.assertResponse(accept_friend_request(user2, method='GET'), 200, count=1)
+        self.assertFriendResponse(create_friendship(user1, user2))
+        self.assertResponse(get_friends(user2), 200, count=1)
 
         self.assertResponse(blocked_user(user1, user2['username']), 201)
 
@@ -70,47 +71,47 @@ class Test02_FriendRequest(UnitTest):
     def test_002_user_does_not_exist(self):
         user1 = new_user()
 
-        self.assertResponse(send_friend_request(user1, {'username': 'caca'}), 404, {'detail': 'User not found.'})
+        self.assertResponse(friend_requests(user1, {'username': 'caca'}), 404, {'detail': 'User not found.'})
 
     def test_003_already_request(self):
         user1 = new_user()
         user2 = new_user()
 
-        self.assertResponse(send_friend_request(user1, user2), 201)
-        self.assertResponse(send_friend_request(user1, user2), 409, {'detail': 'You have already sent a friend request to this user.'})
-        self.assertResponse(send_friend_request(user2, user1), 409, {'detail': 'You have already received a friend request from this user.'})
+        self.assertResponse(friend_requests(user1, user2), 201)
+        self.assertResponse(friend_requests(user1, user2), 409, {'detail': 'You have already sent a friend request to this user.'})
+        self.assertResponse(friend_requests(user2, user1), 409, {'detail': 'You have already received a friend request from this user.'})
 
     def test_004_already_friends(self):
         user1 = new_user()
         user2 = new_user()
 
-        self.assertFriendResponse(create_friend(user1, user2))
-        self.assertResponse(send_friend_request(user1, user2), 409, {'detail': 'You are already friends with this user.'})
+        self.assertFriendResponse(create_friendship(user1, user2))
+        self.assertResponse(friend_requests(user1, user2), 409, {'detail': 'You are already friends with this user.'})
 
     def test_005_send_friend_request_then_blocked(self):
         user1 = new_user()
         user2 = new_user()
 
-        self.assertResponse(send_friend_request(user1, user2), 201)
+        self.assertResponse(friend_requests(user1, user2), 201)
         self.assertResponse(blocked_user(user2, user1['username']), 201)
-        self.assertResponse(receive_friend_requests(user2), 200, count=0)
-        self.assertResponse(send_friend_request(user1, method='GET'), 200, count=0)
+        self.assertResponse(get_friend_requests_received(user2), 200, count=0)
+        self.assertResponse(friend_requests(user1, method='GET'), 200, count=0)
 
     def test_006_blocked_then_send_friend_request(self):
         user1 = new_user()
         user2 = new_user()
 
         self.assertResponse(blocked_user(user1, user2['username']), 201)
-        self.assertResponse(send_friend_request(user1, user2), 403, {'detail': 'You blocked this user.'})
-        self.assertResponse(send_friend_request(user2, user1), 404, {'detail': 'User not found.'})
+        self.assertResponse(friend_requests(user1, user2), 403, {'detail': 'You blocked this user.'})
+        self.assertResponse(friend_requests(user2, user1), 404, {'detail': 'User not found.'})
 
     def test_007_send_friend_request_to_myself(self):
         user1 = new_user()
 
-        self.assertResponse(send_friend_request(user1, user1), 403, {'detail': 'You cannot send a friend request to yourself.'})
+        self.assertResponse(friend_requests(user1, user1), 403, {'detail': 'You cannot send a friend request to yourself.'})
 
     def test_008_forget_username_field(self):
-        self.assertResponse(send_friend_request(data={}), 400, {'username': ['This field is required.']})
+        self.assertResponse(friend_requests(data={}), 400, {'username': ['This field is required.']})
 
 
 if __name__ == '__main__':
