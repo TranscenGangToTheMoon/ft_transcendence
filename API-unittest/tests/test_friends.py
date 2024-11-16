a073a00ceb1492492a3eab1b74c174d3accba828
fguirama <florian.guiramand@protonmail.com>
2024-11-16 14:52:24 +0100

    make test_005_get_friends

--- API-unittest/tests/test_friends.py
+++ API-unittest/tests/test_friends.py
@@ -22,6 +22,16 @@ class Test01_Friend(UnitTest):
         self.assertFriendResponse(create_friend(user1, user2))
         self.assertResponse(accept_friend_request(user2, user1), 409, {'detail': 'You are already friends with this user.'})
 
+    def test_005_get_friends(self):
+        user1 = new_user()
+
+        for i in range(7):
+            self.assertFriendResponse(create_friend(user1))
+
+        response = accept_friend_request(user1, method='GET')
+        self.assertResponse(response, 200)
+        self.assertEqual(7, response.json['count'])
+
 
 if __name__ == '__main__':
     unittest.main()
