b63da2615ac751917b2435de62c033fece0f0a3e
fguirama <florian.guiramand@protonmail.com>
2024-11-16 14:51:23 +0100

    import all needed function

--- API-unittest/tests/test_friends.py
+++ API-unittest/tests/test_friends.py
@@ -1,3 +1,7 @@
+from services.blocked import blocked_user
+from services.friend import create_friend, accept_friend_request, send_friend_request
+from utils.credentials import new_user
+from utils.my_unittest import UnitTest
 
 # 1. todo make test
 class Test01_Friend(UnitTest):
