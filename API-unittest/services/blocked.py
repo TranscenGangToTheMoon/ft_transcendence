fdfef62502ee1b4a67eb3661a2e89514403b55d1
fguirama <florian.guiramand@protonmail.com>
2024-11-30 17:46:10 +0100

    add args method and data in blocked_user

--- API-unittest/services/blocked.py
+++ API-unittest/services/blocked.py
@@ -1,11 +1,22 @@
+from typing import Literal
+
+from utils.credentials import new_user
 from utils.request import make_request
 
 
-def blocked_user(user, user_id):
+def blocked_user(user, user_id=None, data=None, method: Literal['GET', 'POST'] = 'POST'):
+    if data is None:
+        if method == 'POST':
+            if user_id is None:
+                user_id = new_user()['id']
+            data = {'user_id': user_id}
+        else:
+            data = {}
+
     return make_request(
         endpoint='users/me/blocked/',
-        method='POST',
-        data={'user_id': user_id},
+        method=method,
+        data=data,
         token=user['token'],
     )
 
