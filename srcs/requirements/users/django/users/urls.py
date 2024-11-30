c297cdf128bcaf471df3cc4e4f19116ecc4925cd
fguirama <florian.guiramand@protonmail.com>
2024-11-30 23:26:53 +0100

    make todo

--- srcs/requirements/users/django/users/urls.py
+++ srcs/requirements/users/django/users/urls.py
@@ -27,27 +27,13 @@ urlpatterns = [
 
 # todo handle delete user
 # todo handle rename user
-
-# todo make endpoint for user stats
-# todo make endpoint for user xp
-# todo make endpoint for user trophies
-# todo make endpoint for coins
+# todo make endpoint for user stats, xp, trophies (when match ended, or when tournament ended)
 # todo rename all view for more comprehensive name
 # todo make endpoint for online status
-# todo make endpoint for user rank
-# todo remake user auth update value (is_guest and username)
-# todo make endpoint for match history
- # todo WHEN MAKE DA - make also test at the same time
- # todo test all endpoint
- # todo make doc for all services
- # todo fix github issue
+# todo fix github issue
 # todo create chat when lobby or tournament is created
 # todo delete chat after finish tournament
 # todo delete chat after finish lobby
 # todo make test for that
 # todo make doc for all endpoint
-#todo make season
-# todo make ranks
-#todo make request when a user win a tournament (for coins and xp win)
-# todo see new message in chat preview
-# todo when guest ca fait longtemps que pas connecter, delete guest (just check si jamais, il est dans un match)
+# todo make season
