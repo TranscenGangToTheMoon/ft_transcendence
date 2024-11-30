aed8c68fe5fd6495bbca14cfa01961fb65ba726d
fguirama <florian.guiramand@protonmail.com>
2024-11-30 23:03:15 +0100

    remove comment INSTALLED_APPS

--- srcs/requirements/matchmaking/django/matchmaking/settings.py
+++ srcs/requirements/matchmaking/django/matchmaking/settings.py
@@ -35,8 +35,6 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-	# 'daphne',
-	# 'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
