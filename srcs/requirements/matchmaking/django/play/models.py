82e61645417c9b11027ec4a56d01b687302c15fb
fguirama <florian.guiramand@protonmail.com>
2024-12-01 23:20:26 +0100

    add game_mode field todo

--- srcs/requirements/matchmaking/django/play/models.py
+++ srcs/requirements/matchmaking/django/play/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Players(models.Model):
     user_id = models.IntegerField(unique=True)
     trophies = models.IntegerField()
-    game_mode = models.CharField(max_length=50) # duel, ranked
+    game_mode = models.CharField(max_length=10) # todo try to set available option right here (not in serializer)
     join_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
