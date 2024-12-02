35a7aabb76305b58600d38867127a3910f51899e
fguirama <florian.guiramand@protonmail.com>
2024-12-03 00:55:08 +0100

    surcharge __str__ in Blocked model

--- srcs/requirements/matchmaking/django/blocking/models.py
+++ srcs/requirements/matchmaking/django/blocking/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Blocked(models.Model):
     user_id = models.IntegerField()
     blocked_user_id = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.user_id} blocked {self.blocked_user_id}'
