b5039f1aa232ba45ded359c09427278263e53c92
fguirama <florian.guiramand@protonmail.com>
2024-12-03 15:40:38 +0100

    remove ROTATE_REFRESH_TOKENS and BLACKLIST_AFTER_ROTATION

--- srcs/requirements/auth/django/auth/settings.py
+++ srcs/requirements/auth/django/auth/settings.py
@@ -149,7 +149,5 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ['Bearer'],
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=3),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
 }
