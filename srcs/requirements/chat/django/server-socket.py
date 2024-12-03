39571be327fd85711d50c29248cea95fe52c3f53
Liurne <jules.coquard@gmail.com>
2024-12-03 18:24:47 +0100

    django started by server-socket.py, event handler good, need help for auth_verif may change for user me

--- srcs/requirements/chat/django/server-socket.py
+++ srcs/requirements/chat/django/server-socket.py
@@ -1,7 +1,20 @@
 
+import os
+import sys
+import django
 import socketio
 import asyncio
 from aiohttp import web
+from lib_transcendence.auth import auth_verify
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
+
+django.setup()
+
+from chats.models import Chats, ChatParticipants
 
 sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='aiohttp', logger=True)
 app = web.Application()
@@ -9,28 +22,36 @@ sio.attach(app, socketio_path='/ws/chat/')
 
 @sio.event
 async def connect(sid, environ):
-    print(f"Client connected : {sid}")
-    await sio.emit('message', {'message': '<em>Welcome!</em>',}, to=sid)
+    print(f"Client attempting to connect : {sid}")
+    token = environ.get('HTTP_TOKEN')
+    chat_id = environ.get('HTTP_CHATID')
+    await sio.emit('debug', {'token': token, 'chat_id': chat_id}, to=sid)
+    if token and chat_id:
+        try:
+            print("trying to auth")
+            res = auth_verify(token)
+            await sio.emit('message', {'message': "<em>Authentification validated!</em>"}, to=sid)
+        except Exception as e:
+            print("token error")
+            await sio.emit('message', {'message': '<em>Invalid token!</em>',}, to=sid)
+            # await sio.disconnect(sid)
+        print(f"Connection successeeded{sid}")
+        try:
+            test = Chats.objects.get(id=chat_id)
+            print(test)
+            await sio.emit('debug', {test}, to=sid)
+        except Exception as e:
+            print(e)
+        await sio.emit('message', {'message': '<em>Welcome!</em>',}, to=sid)
+    else:
+        print(f"Connection failed{sid}")
+        await sio.emit('message', {'message': '<em>missing args!</em>',}, to=sid)
+        # await sio.disconnect(sid)
 
 
 @sio.event
 async def disconnect(sid):
     print(f"Client disconnected : {sid}")
-
-@sio.event
-async def auth(sid, data):
-    token = data.get('token')
-    chatID = data.get('chatID')
-    print(chatID, token)
-    await sio.emit('debug', {'chatID': chatID, 'token':token}, to=sid)
-    if chatID and token:
-        print(f"New auth from {sid}: {data}")
-        await sio.emit('message', {'message': "<em>Authentification validated!</em>"}, to=sid)
-        request_auth()
-    else:
-        await sio.emit('error', {'message': "<em>Euuuuh who are you ?!!! Acces DENIED !</em>"}, to=sid)
-        await asyncio.sleep(0.1);
-        await sio.disconnect(sid)
 
 @sio.event
 async def message(sid, data):
--- srcs/requirements/frontend/static/chatTemplates/scripts/chatClient.js
+++ srcs/requirements/frontend/static/chatTemplates/scripts/chatClient.js
@@ -1,79 +1,19 @@
-const chatBox = document.getElementById('chats-box');
-let socket;
-let chatId = 1;
-
-
-function connect() {
-	socket = io("wss://localhost:4443", {
-		path: "/ws/chat/"
+function connect(chatId) {
+	let socket = io("wss://localhost:4443", {
+		path: "/ws/chat/",
+		extraHeaders: {
+			"token": getAccessToken(),
+			"chatId": chatId
+		}
 	});
+	window.socket = socket;
 	console.log("Connecting to the server...");
-	document.getElementById('chat-start-form').style.display = 'none';
-	document.getElementById('chat-view').style.display = 'block';
 	setupSocketListeners();
-}
-
-function disconnect() {
-	socket.disconnect();
-	console.log("Closing the connection to the server...");
-	document.getElementById('chat-start-form').style.display = 'block';
-	document.getElementById('chat-view').style.display = 'none';
-}
-
-function authentificate() {
-	let authForm = document.getElementById('auth-form');
-	authForm.addEventListener('submit', (e) => {
-		e.preventDefault();
-		const chatID = e.target.chatID.value;
-		console.log('chatID: ', chatID);
-		socket.emit('auth', { 'token':getAccessToken(), 'chatID': chatID });
-		console.log('Authentification in progress...');
-		authForm.reset();
-	});
+	return true;
 }
 
 
-function startchat() {
-	let chatStartForm = document.getElementById('chat-start-form');
-	chatStartForm.addEventListener('submit', (e) => {
-		e.preventDefault();
-		connect();
-	});
-}
-
-function endchat() {
-	let chatEndForm = document.getElementById('chat-end-form');
-	chatEndForm.addEventListener('submit', (e) => {
-		e.preventDefault();
-		disconnect();
-	});
-}
-
-function setupSocketListeners()
-{
-	socket.on("connect", () => {
-		console.log("Connected to the server");
-	});
-
-	socket.on("disconnect", () => {
-		console.log("Disconnected from the server");
-		//chatBox.innerHTML = "";
-	});
-
-	socket.on("message", (data) => {
-		console.log("Message received: ", data);
-		chatBox.insertAdjacentHTML('beforeend', `<div><p>: ${data.message}</p></div>`);
-	});
-
-	socket.on("error", (data) => {
-		console.log("Error received: ", data);
-		disconnect();
-	});
-
-	socket.on("debug", (data) => {
-		console.log("Debug received: ", data);
-	});
-
+function send() {
 	let chatForm = document.getElementById('chat-form');
 	chatForm.addEventListener('submit', (e) => {
 		e.preventDefault();
@@ -82,8 +22,56 @@ function setupSocketListeners()
 		console.log('Message sent: ', message);
 		chatForm.reset();
 	});
-	authentificate();
-	endchat();
+}
+
+
+function setupSocketListeners()
+{
+	document.getElementById('endChat').addEventListener('click', () => {
+		socket.disconnect();
+	});
+	document.getElementById('logOut').addEventListener('click', () => {
+		socket.disconnect();
+	});
+
+	const chatBox = document.getElementById('chats-box');
+
+	socket.off("connect");
+    socket.off("disconnect");
+    socket.off("message");
+    socket.off("error");
+	socket.off("debug");
+	
+	socket.on("connect", () => {
+		console.log("Connected to the server");
+		document.getElementById('startChat').style.display = 'none';
+		document.getElementById('chat-view').style.display = 'block';
+	});
+	
+	socket.on("disconnect", () => {
+		console.log("Disconnected from the server");
+		console.log("Closing the connection to the server...");
+		document.getElementById('startChat').style.display = 'block';
+		document.getElementById('chat-view').style.display = 'none';
+		chatBox.innerHTML = "";
+		socket = null;
+	});
+	
+	socket.on("message", (data) => {
+		console.log("Message received: ", data);
+		chatBox.insertAdjacentHTML('beforeend', `<div><p>: ${data.message}</p></div>`);
+	});
+	
+	socket.on("error", (data) => {
+		console.log("Error received: ", data);
+		socket.disconnect();
+	});
+	
+	socket.on("debug", (data) => {
+		console.log("Debug received: ", data);
+	});
+
+	send();
 }
 
 async function chatClientInit() {
@@ -91,4 +79,10 @@ async function chatClientInit() {
 }
 
 chatClientInit();
-startchat();
+
+document.getElementById('startChat').addEventListener('submit', (e) => {
+	e.preventDefault();
+	const chatId = e.target.chatId.value;
+	connect(chatId)
+	document.getElementById('startChat').reset();
+})
