628fc1e04cc589795acd26a9c2a03d166324f084
fguirama <florian.guiramand@protonmail.com>
2024-12-06 02:17:11 +0100

    handle 'POST' method in RegisterSerializer

--- srcs/requirements/auth/django/register/serializers.py
+++ srcs/requirements/auth/django/register/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from lib_transcendence.exceptions import MessagesException
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -7,7 +8,7 @@ from guest.group import get_group_guest
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(validators=[validate_username], write_only=True)
+    username = serializers.CharField(write_only=True)
     password = serializers.CharField(write_only=True)
     access = serializers.CharField(read_only=True)
     refresh = serializers.CharField(read_only=True)
@@ -20,6 +21,19 @@ class RegisterSerializer(serializers.ModelSerializer):
             'access',
             'refresh',
         ]
+
+    def validated_username(self, value):
+        request = self.context.get('request')
+
+        if request is None:
+            raise serializers.ValidationError(MessagesException.ValidationError.REQUEST_REQUIRED)
+
+        return validate_username(value, request.method == 'POST')
+
+    def create(self, validated_data):
+        instance = super().create(validated_data)
+        refresh_token = RefreshToken.for_user(instance)
+        return {'access': str(refresh_token.access_token), 'refresh': str(refresh_token)}
 
     def update(self, instance, validated_data):
         guest_group = get_group_guest()
