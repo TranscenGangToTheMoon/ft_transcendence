8cacb069ac7404972121598585d93e2148cc9480
fguirama <florian.guiramand@protonmail.com>
2024-12-06 02:16:49 +0100

    update validate_username

--- srcs/requirements/auth/django/auth/validators.py
+++ srcs/requirements/auth/django/auth/validators.py
@@ -8,15 +8,16 @@ from django.contrib.auth.models import User
 valid_charset = ascii_letters + digits + '_-'
 
 
-def validate_username(value):
-    if value in ('anonymous', 'admin', 'staff'):
-        raise serializers.ValidationError(MessagesException.ValidationError.USERNAME_NOT_ALLOWED)
-    if len(value) < 3:
-        raise serializers.ValidationError(MessagesException.ValidationError.USERNAME_LONGER_THAN_3_CHAR)
-    if len(value) > 30:
-        raise serializers.ValidationError(MessagesException.ValidationError.USERNAME_SHORTER_THAN_30_CHAR)
-    if any(char not in valid_charset for char in value):
-        raise serializers.ValidationError(MessagesException.ValidationError.INVALIDE_CHAR)
-    if User.objects.filter(username__iexact=value).exists():
+def validate_username(value, check_exists=True, only_check_exists=False):
+    if not only_check_exists:
+        if value in ('anonymous', 'admin', 'staff'):
+            raise serializers.ValidationError(MessagesException.ValidationError.USERNAME_NOT_ALLOWED)
+        if len(value) < 3:
+            raise serializers.ValidationError(MessagesException.ValidationError.USERNAME_LONGER_THAN_3_CHAR)
+        if len(value) > 30:
+            raise serializers.ValidationError(MessagesException.ValidationError.USERNAME_SHORTER_THAN_30_CHAR)
+        if any(char not in valid_charset for char in value):
+            raise serializers.ValidationError(MessagesException.ValidationError.INVALIDE_CHAR)
+    if (check_exists or only_check_exists) and User.objects.filter(username__iexact=value).exists():
         raise serializers.ValidationError(MessagesException.ValidationError.USERNAME_ALREAY_EXISTS)
     return value
