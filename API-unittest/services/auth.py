df5cc6d47c053cee42793bf8c5723802ef901a91
fguirama <florian.guiramand@protonmail.com>
2024-12-07 00:27:51 +0100

    make register_guest

--- API-unittest/services/auth.py
+++ API-unittest/services/auth.py
@@ -14,14 +14,22 @@ def register(username=None, password=None, data=None, method='POST'):
     )
 
 
-    kwargs = {
-        'endpoint': 'auth/register/',
-        'method': method,
-        'data': data
-    }
+def register_guest(guest=None, username=None, password=None, data=None, method='PATCH', token=None):
+    if data is None:
+        if guest is None:
+            data = {'username': username, 'password': password}
+        else:
+            data = {'username': guest['username'], 'password': 'password_' + guest['username']}
 
-    if guest is not None and method == 'PATCH':
-        kwargs['token'] = guest['token']
-        kwargs['data'] = {'username': guest['username'], 'password': 'password_' + guest['username']}
+    kwargs = {
+        'endpoint': 'auth/register/guest/',
+        'method': method,
+        'data': data,
+    }
+    if token is None:
+        if guest is not None:
+            kwargs['token'] = guest['token']
+    else:
+        kwargs['token'] = token
 
     return make_request(**kwargs)
