b916fa1526b71feae79648c03565e5877471436d
fguirama <florian.guiramand@protonmail.com>
2024-12-07 00:29:50 +0100

    add RegisterGuestView and GuestRequiredPermission

--- srcs/requirements/auth/django/register/views.py
+++ srcs/requirements/auth/django/register/views.py
@@ -6,13 +6,23 @@ from guest.group import is_guest
 from register.serializers import RegisterSerializer
 
 
-class RegisterView(generics.UpdateAPIView, generics.CreateAPIView):
+class GuestRequiredPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and is_guest(request)
+
+
+class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+    authentication_classes = []
+
+
+class RegisterGuestView(generics.UpdateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [GuestRequiredPermission]
 
     def get_object(self):
-        if self.request.user.is_authenticated and not is_guest(self.request):
-            raise PermissionDenied(MessagesException.PermissionDenied.GUEST_REQUIRED)
         return self.request.user
 
 
 register_view = RegisterView.as_view()
+register_guest_view = RegisterGuestView.as_view()
