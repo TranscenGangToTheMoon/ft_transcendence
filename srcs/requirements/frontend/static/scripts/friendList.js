b7fc93c8a59b619c9b771e86570acfef1ed8157d
limastic <faust.nino@gmail.com>
2024-12-07 13:41:53 +0100

    added friendship deletion button

--- srcs/requirements/frontend/static/scripts/friendList.js
+++ srcs/requirements/frontend/static/scripts/friendList.js
@@ -27,7 +27,6 @@ if (document.getElementById('modals').friendListened !== true){
     })
 }
 
-
 // document.getElementById('sendFriendRequest').addEventListener('click', async event => {
 //     console.log('salut')
 //     event.preventDefault();
@@ -52,10 +51,12 @@ async function friendListInit(){
                 resultDiv.innerText = "you don't have any friends";
             else{
                 resultDiv.innerText = "";
-                for (let friend of data.results){
+                for (let friend of data.results){   
                     const friend1 = friend.friends[0];
                     const friend2 = friend.friends[1];
-                    resultDiv.innerHTML += `<div>${friend1 === userInformations.username ? friend2 : friend1}</div>\n`;
+                    resultDiv.innerHTML += `<div class="friendRequestBlock" id="friend${friend.id}">\
+                    <div>${friend1 === userInformations.username ? friend2 : friend1}</div>\
+                    <button class='deleteFriend'>delete X</button></div>\n`;
                 }
                 // document.getElementById('knownFriends').innerText = "unable to fetch friend list";
             }
@@ -101,4 +102,26 @@ async function friendListInit(){
     }
 }
 
-friendListInit();
+
+(async function deleteFriend(){
+    await friendListInit();
+    const deleteFriendButtons = document.querySelectorAll('.deleteFriend');
+    console.log(deleteFriendButtons);
+    for (let deleteFriendButton of deleteFriendButtons){
+        if (!deleteFriendButton.listened){
+            deleteFriendButton.addEventListener('click', async event => {
+                try {
+                    const friendshipId = `${deleteFriendButton.parentElement.id}`.substring(6);
+                    let data = await apiRequest(getAccessToken(), `${baseAPIUrl}/users/me/friends/${friendshipId}/`, 'DELETE');
+                    console.log('deleted friendship : ', friendshipId);
+                    await friendListInit();
+
+                }
+                catch (error){
+                    console.log(error);
+                }
+            })
+        }
+        deleteFriendButton.listened = true;
+    }
+})()
