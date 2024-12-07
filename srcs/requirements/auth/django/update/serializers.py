1761592467c708eaae70c6d87b82ff8949d4c8a0
fguirama <florian.guiramand@protonmail.com>
2024-12-07 22:20:35 +0100

    add check SAME_PASSWORD

--- srcs/requirements/auth/django/update/serializers.py
+++ srcs/requirements/auth/django/update/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from lib_transcendence.exceptions import MessagesException
 
 from auth.validators import validate_username
 
@@ -14,6 +15,8 @@ class UpdateSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         password = validated_data.pop('password', None)
+        if instance.check_password(password):
+            raise serializers.ValidationError({'password': [MessagesException.ValidationError.SAME_PASSWORD]})
         if password is not None:
             instance.set_password(password)
         return super().update(instance, validated_data)
