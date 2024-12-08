a585c3ac5cbc3eaecbd5c65a96ad821591a87e68
fguirama <florian.guiramand@protonmail.com>
2024-12-08 19:17:33 +0100

    make connect_to_sse

--- API-unittest/services/sse.py
+++ API-unittest/services/sse.py
@@ -1,5 +1,19 @@
-from utils.request import make_request
+import httpx
+
+from utils.credentials import new_user
 
 
-def sse(user):
-    make_request('users/me/sse/', token=user['token'])
+def connect_to_sse(user=None):
+    sse_url = "https://localhost:4443/api/users/me/sse/"
+    if user is None:
+        user = new_user()
+
+    with httpx.Client(verify=False) as client:
+        with client.stream("GET", sse_url, headers={'Authorization': f'Bearer {user["token"]}'}) as response:
+            if response.status_code == 200:
+                print("Connected to SSE server.")
+                for line in response.iter_text():
+                    if line.strip():
+                        print(f"Received: {line.strip()}")
+            else:
+                print(f"Failed to connect: {response.status_code}")
