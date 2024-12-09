cda137ae5417da62a163dfb4973ba994067ea9fa
xcharra <xcharra@student.42lyon.fr>
2024-12-09 18:48:41 +0100

    Some test to filling profile in main menu page

--- srcs/requirements/pong-cli/srcs/main.cpp
+++ srcs/requirements/pong-cli/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: xcharra <xcharra@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 16:32:19 by xcharra           #+#    #+#             */
-/*   Updated: 2024/12/06 17:28:04 by xcharra          ###   ########.fr       */
+/*   Updated: 2024/12/09 18:43:52 by xcharra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,19 +30,75 @@ int main(void)
 		PongCLI		app(curl, user);
 
 		curl.addHeader("Content-Type: application/json");
-//		app.run();
-		app.changePage(PongCLI::Page::MainMenuPage);
+		app.run();
+//		app.changePage(PongCLI::Page::MainMenuPage);
 
 		std::cout << "server: " << app.getServer() << std::endl;
 		std::cout << "username: " << app.getUsername() << std::endl;
 		std::cout << "password: " << app.getPassword() << std::endl;
+	}
+	{
+//		CurlWrapper	curl("https://localhost:4443");
+//		User		user;
+//
+//		curl.addHeader("Content-Type: application/json");
+//		//login test test
+//		user.setUsername("test");
+//		user.setPassword("test");
+//		try {
+//			user.loginUser(curl);
+//		}
+//		catch (std::exception &error) {
+//			std::cerr << ("(" + std::to_string(curl.getHTTPCode()) + ") " + error.what()) << std::endl;
+//		}
+//
+////		int			id = 9;
+//		std::string	username;
+//		bool		guest;
+////	profile_picture: None, // not handle yet
+////	status: None, // not handle yet
+////		int			stardust = 12996;
+////		int			aura = 1200;
+////	current_rank: None, // not handle yes
+////	friends: None,
+//		bool		accept_friend_request;
+//		bool		accept_chat_from;
+//
+//		Component	pageComponents = Container::Vertical({
+//		});
+//		std::string	json;
+//		std::string info;
+//		try {
+//			curl.addHeader("Authorization: Bearer " + user.getAccessToken());
+//			curl.GET("/api/users/me/", "");
+//			json = curl.getResponse();
+//			std::cerr << json << std::endl;
+//			info = "id";
+////			id = std::stoi(jsonParser(json, "id"));
+//			info = "username";
+//			username = jsonParser(json, "username");
+//			info = "is_guest";
+//			guest = jsonParser(json, "is_guest") == "true";
+//			info = "coins";
+////			stardust = std::stoi(jsonParser(json, "coins"));
+//			info = "trophies";
+////			aura = std::stoi(jsonParser(json, "trophies"));
+//			info = "accept_friend_request";
+//			accept_friend_request = jsonParser(json, "accept_friend_request") == "true";
+//			info = "accept_chat_from";
+//			accept_chat_from = jsonParser(json, "accept_chat_from") == "true";
+//			//request to /api/user/me >> get user information
+//		}
+//		catch (std::exception &error) {
+//			std::cerr << ("(" + std::to_string(curl.getHTTPCode()) + ") " + error.what() + info) << std::endl;
+//		}
 	}
 	return (0);
 }
 
 //std::string generateCustomID(size_t length) {
 //	const char charset[] = "0123456789abcdef";
-//	const size_t charsetSize = sizeof(charset) - 1;
+//	const s	xize_t charsetSize = sizeof(charset) - 1;
 //
 //	std::random_device				rd;
 //	std::mt19937					gen(rd());
