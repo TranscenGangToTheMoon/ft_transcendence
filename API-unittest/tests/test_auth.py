87d9a8ecc74b28b43df22b160a96d8309c41286a
fguirama <florian.guiramand@protonmail.com>
2024-12-11 19:05:39 +0100

    make todo

--- API-unittest/tests/test_auth.py
+++ API-unittest/tests/test_auth.py
@@ -8,12 +8,7 @@ from utils.generate_random import rnstr
 from utils.my_unittest import UnitTest
 
 
-# todo test register
-# todo test register invalid name
-# todo test register invalid password
 # todo test refresh
-# todo test get token
-# todo test guest
 
 class Test01_Register(UnitTest):
 
