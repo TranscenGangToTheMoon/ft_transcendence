bc6eec45819bce9a18e003f0e9e4f5f18a4eb9f4
Basile Jeannot <bajeanno@student.42lyon.fr>
2024-12-11 23:41:09 +0100

    formatted socketio data emits

--- srcs/requirements/game/django/game_server/io_handlers.py
+++ srcs/requirements/game/django/game_server/io_handlers.py
@@ -1,51 +1,64 @@
+from logging import info, debug, warning, error
+
 
 async def connect(sid, environ, auth):
     from game_server.server import Server
-    #print(Server.sio, flush=True)
-    #print('trying to connect', flush=True)
-    if auth['token'] == 'kk':
-        print(f"Client connecté : {sid}", flush=True)
-    else:
+
+    # Websockets Debug entrypoint
+    if auth['token'] == 'debug': # TODO -> remove this in prod
+        debug('Debug client connected')
+        return True
+
+    # TODO -> do authentication here
+    if auth['token'] != 'kk':
         raise ConnectionRefusedError('Authentication failed')
     id = auth['id']
-    #print('id: ', id, flush=True)
     try:
         player, match_code = Server.get_player_and_match_code(id)
-        #print('got player and its match_code', flush=True)
         player.socket_id = sid
         Server.clients[sid] = player
+        info('Transport is : ', Server.sio.transport(sid))
         await Server.sio.enter_room(sid, str(match_code))
     except Exception as e:
-        #print(e, flush=True)
+        error(e)
         raise ConnectionRefusedError('Player does not belong to any game')
-    #print('registered a new racket', flush=True)
+    info('Client connected & authenticated')
 
 
 async def move_up(sid):
     from game_server.server import Server
     player = Server.clients[sid]
-    #print(f'caaccaacca {player}', flush=True)
-    #print(f'caaccaacca {player.racket}', flush=True)
     player.racket.move_up()
     await Server.sio.emit(
         'move_up',
         data={'player': player.user_id},
         room=str(player.match_code),
-        skip_sid=sid)
+        skip_sid=sid
+    )
 
 
 async def move_down(sid):
     from game_server.server import Server
     player = Server.clients[sid]
     player.racket.move_down()
-    await Server.sio.emit('move_down', data={'player': player.user_id}, room=str(player.match_code), skip_sid=sid)
+    await Server.sio.emit(
+        'move_down',
+        data={'player': player.user_id},
+        room=str(player.match_code),
+        skip_sid=sid
+    )
 
 
 async def stop_moving(sid):
     from game_server.server import Server
     player = Server.clients[sid]
     player.racket.stop_moving()
-    await Server.sio.emit('stop_moving', data={'player': player.user_id}, room=str(player.match_code), skip_sid=sid)
+    await Server.sio.emit(
+        'stop_moving',
+        data={'player': player.user_id},
+        room=str(player.match_code),
+        skip_sid=sid
+    )
 
 
 async def send_games(sid):
