9c4bdc60ef136c7e2d8146f45f29b71f0acb1afa
fguirama <florian.guiramand@protonmail.com>
2024-12-15 16:54:57 +0100

    make todo

--- srcs/requirements/users/django/users/urls.py
+++ srcs/requirements/users/django/users/urls.py
@@ -34,7 +34,6 @@ urlpatterns = [
     path(UsersManagement.manage_user, manage_user_view),
 ]
 
-# todo get user instance in chat
 # todo get user instance in lobby and tournament
 # todo get user instance in tournament
 # todo 2 truc basile (endpoint)
