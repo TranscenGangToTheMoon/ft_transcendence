7240f017a13b8ab3eb70dcd3543e87d744513cbe
fguirama <florian.guiramand@protonmail.com>
2024-12-15 23:31:37 +0100

    make todo

--- API-unittest/tests/test_game.py
+++ API-unittest/tests/test_game.py
@@ -4,14 +4,6 @@ from services.game import create_game, is_in_game
 from utils.credentials import new_user
 from utils.my_unittest import UnitTest
 
-# todo test
-# todo test already in game
-# todo test game
-# todo test not same nb of user in team
-# todo test not team
-# todo test invalide team
-# todo test invlaid game mode
-
 
 class Test01_Game(UnitTest):
 
