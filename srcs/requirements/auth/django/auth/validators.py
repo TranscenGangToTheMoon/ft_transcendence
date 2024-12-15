f78627582e3e6bee7e48278524cded39e1585279
fguirama <florian.guiramand@protonmail.com>
2024-12-15 22:18:36 +0100

    make set_password
    This function validate password and set if is good. It's also save user instance

--- srcs/requirements/auth/django/auth/validators.py
+++ srcs/requirements/auth/django/auth/validators.py
@@ -1,11 +1,14 @@
 from string import ascii_letters, digits
 
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from lib_transcendence.exceptions import MessagesException
 from django.contrib.auth.models import User
 
 
 valid_charset = ascii_letters + digits + '_-'
+valid_password_charset = valid_charset + '!@#$%^&*()'
 
 
 def validate_username(value, check_exists=True, only_check_exists=False):
@@ -23,3 +26,22 @@ def validate_username(value, check_exists=True, only_check_exists=False):
     if (check_exists or only_check_exists) and User.objects.filter(username__iexact=value).exists():
         raise serializers.ValidationError(MessagesException.ValidationError.USERNAME_ALREAY_EXISTS)
     return value
+
+
+def set_password(password, user, remove_instance=False, check_previous_password=False):
+    if password is None:
+        return
+    try:
+        if check_previous_password and user.check_password(password):
+            raise ValidationError(MessagesException.ValidationError.SAME_PASSWORD)
+        if len(password) > 50:
+            raise ValidationError(MessagesException.ValidationError.PASSWORD_SHORTER_THAN_50_CHAR)
+        if any(char not in valid_password_charset for char in password):
+            raise ValidationError(MessagesException.ValidationError.INVALIDE_CHAR)
+        validate_password(password, user)
+    except ValidationError as e:
+        if remove_instance:
+            user.delete()
+        raise serializers.ValidationError({'password': e.messages})
+    user.set_password(password)
+    user.save()
