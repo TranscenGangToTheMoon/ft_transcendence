428eda1a2971137239203f40bb2c3a6bf2ee08a1
fguirama <florian.guiramand@protonmail.com>
2024-12-15 16:51:59 +0100

    rename chat in auth service not in user

--- srcs/requirements/auth/django/update/serializers.py
+++ srcs/requirements/auth/django/update/serializers.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import User
-from rest_framework import serializers
+from lib_transcendence import endpoints
 from lib_transcendence.exceptions import MessagesException
+from lib_transcendence.services import request_chat
+from rest_framework import serializers
+from rest_framework.exceptions import APIException
 
 from auth.validators import validate_username
+from guest.group import is_guest
 
 
 class UpdateSerializer(serializers.ModelSerializer):
@@ -19,4 +23,9 @@ class UpdateSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({'password': [MessagesException.ValidationError.SAME_PASSWORD]})
         if password is not None:
             instance.set_password(password)
+        if 'username' in validated_data and not is_guest(user=instance):
+            try:
+                request_chat(endpoints.UsersManagement.frename_user.format(user_id=instance.id), data={'username': validated_data['username']})
+            except APIException:
+                pass
         return super().update(instance, validated_data)
