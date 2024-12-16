c30917c9fcad110a71c6d02f22656dac5d6ce326
xcharra <xcharra@student.42lyon.fr>
2024-12-16 19:37:22 +0100

    WIP dont Work

--- srcs/requirements/pong-cli/srcs/main.cpp
+++ srcs/requirements/pong-cli/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: xcharra <xcharra@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 16:32:19 by xcharra           #+#    #+#             */
-/*   Updated: 2024/12/16 13:17:43 by xcharra          ###   ########.fr       */
+/*   Updated: 2024/12/16 19:35:39 by xcharra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,24 @@
 #include "CurlWrapper.hpp"
 #include "PongCLI.hpp"
 #include "User.hpp"
+#include "asio.hpp"
+#include "asio/ssl.hpp"
 
-#define SERVER	"https://localhost:4443"
-
+#define HTTPS_SERVER	"https://localhost:4443"
+#define WSS_SERVER		"wss://localhost:4443"
 int main(void)
 {
 	{
 		CurlWrapper	curl;
 		User		user;
-//		client	client;
+		sio::client		test;
 
-//		client.connect(SERVER);
-		curl.setServer(SERVER);
-		user.setUsername("xcharra");
-		user.setPassword("test");
+		curl.setServer(HTTPS_SERVER);
 		curl.addHeader("Content-Type: application/json");
+
+		user.setUsername("monfiacsfddsdsdddi");
+		user.setPassword("test");
+
 
 		try {
 			user.loginUser(curl);
@@ -54,6 +57,83 @@ int main(void)
 			std::cerr << ("(" + std::to_string(curl.getHTTPCode()) + ") " + error.what()) << std::endl;
 			std::cerr << json::parse(curl.getResponse()).dump(1, '\t') << std::endl;
 		}
+		std::string id;
+		try {
+			curl.GET("/api/users/me/", "");
+			std::cout << "Users me succeed!" << std::endl;
+			std::cout << json::parse(curl.getResponse()).dump(1, '\t') << std::endl;
+			id = std::to_string((int)json::parse(curl.getResponse())["id"]);
+			std::cout << id << std::endl;
+
+		}
+		catch (std::exception &error) {
+			std::cerr << ("(" + std::to_string(curl.getHTTPCode()) + ") " + error.what()) << std::endl;
+			std::cerr << json::parse(curl.getResponse()).dump(1, '\t') << std::endl;
+		}
+//		// Configuration des options de connexion
+//		test.set_open_listener([]() {
+//			std::cout << "Connexion établie avec le serveur !" << std::endl;
+//		});
+//
+//		test.set_fail_listener([]() {
+//			std::cerr << "La connexion a échoué !" << std::endl;
+//		});
+//
+//		test.set_close_listener([](sio::client::close_reason const& reason) {
+//			std::cout << "Connexion fermée, raison: " << static_cast<int>(reason) << std::endl;
+//		});
+
+
+		// Connexion au serveur
+//		std::map<std::string, std::string> options = {
+//			{"id", id},
+//			{"token", "kk"}
+//		};
+//		test.connect("wss://localhost:4443/socket.io/?EIO=3&transport=websocket?token=kk?user_id=" + id);
+//		test.connect("wss://localhost:4443/ws");
+
+//		test.socket()->emit("get_games");
+		// Bloquer pour maintenir la connexion active
+//		std::cin.get();
+		// Déconnexion propre
+//		test.close();
+		asio::io_service	service;
+		asio::ssl::context	ctx(asio::ssl::context::tlsv12);
+		ctx.set_verify_mode(asio::ssl::verify_peer);
+		ctx.load_verify_file("./ft_transcendence.crt");
+
+		asio::ssl::stream<asio::ip::tcp::socket>	socket = new socket(service, ctx);
+//
+		sio::client	client;
+
+		client.socket().reset(socket);
+
+		client.set_open_listener([&]() {
+			std::cout << "Connected to server!" << std::endl;
+		});
+
+		client.set_fail_listener([&]() {
+			std::cout << "Failed to connect to server!" << std::endl;
+		});
+
+		// Connect to a WSS server
+		client.connect("wss://localhost:4443/ws");
+		// Send a message
+		client.socket()->emit("send_games");
+
+		client.socket()->on("games", [&](sio::event& ev) {
+			std::cout << "Received message: " << ev.get_message()->get_string() << std::endl;
+		});
+		// Handle incoming messages
+		client.socket()->on("connect", [](sio::event& ev) {
+			std::cout << "Received message: " << ev.get_message()->get_string() << std::endl;
+		});
+
+		// Keep the client alive
+//		std::this_thread::sleep_for(std::chrono::minutes(10));
+		std::cin.get();
+
+		client.close();
 	}
 	{
 //		CurlWrapper	curl;
