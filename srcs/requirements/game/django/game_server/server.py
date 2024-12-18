9e6df619399b4171d814937358460e615b5fd173
Basile Jeannot <bajeanno@student.42.fr>
2024-12-18 19:40:18 +0100

    deleted monitoring thread

--- srcs/requirements/game/django/game_server/server.py
+++ srcs/requirements/game/django/game_server/server.py
@@ -24,6 +24,14 @@ class Server:
     _loop: asyncio.AbstractEventLoop
 
     @staticmethod
+    def clear_database():
+        time.sleep(2)
+        matches = fetch_matches()
+        for match in matches:
+            if match.finished == False:
+                match.finish_match()
+
+    @staticmethod
     def serve():
         Server._sio = socketio.AsyncServer(async_mode='aiohttp', cors_allowed_origins='*', logger=False)
         Server._app = web.Application()
@@ -39,26 +47,44 @@ class Server:
         Server._games_lock = Lock()
         Server._loop_lock = Lock()
         Server._sio_lock = Lock()
-        port=5500
-        logging.info(f"SocketIO server running on port {port}")
-        # print(f"SocketIO server running on port {port}", flush=True)
-        Server.launch_monitoring()
+        port = 5500
+        '''setting all matches in DB as finished'''
+        Server.clear_database()
+        print(f"SocketIO server running on port {port}", flush=True)
         web.run_app(Server._app, host='0.0.0.0', port=port, loop=Server._loop)
 
     @staticmethod
-    def launch_game(match_code):
-        time.sleep(1)
-        match = fetch_match(match_code)
-        print('launching game with : ')
-        for team in match.teams:
-            for player in team.players:
-                print(player.user_id, flush=True)
-        game = Game(Server._sio, match, Position(800, 600))
-        print(Server._games_lock, flush=True)
+    def delete_game(match_id) -> None:
+        # TODO -> set game as finished (django endpoint)
         Server._games_lock.acquire()
         Server._games.pop(match_id)
         Server._games_lock.release()
-        Server._games[match_code].launch()
+
+    @staticmethod
+    def push_game(match_id, game) -> None:
+        Server._games_lock.acquire()
+        Server._games[match_id] = game
+        Server._games_lock.release()
+
+    @staticmethod
+    def get_game(match_id) -> Game:
+        Server._games_lock.acquire()
+        game = Server._games[match_id]
+        Server._games_lock.acquire()
+        return game
+
+    @staticmethod
+    def launch_game(match_id):
+        time.sleep(1)
+        match = fetch_match(match_id)
+        print('creating game', end='')
+        for team in match.teams:
+            for player in team.players:
+                print(player.user_id, flush=True, end='')
+        print('', flush=True)
+        game = Game(Server._sio, match, Position(800, 600))
+        Server.push_game(match_id, game)
+        Server._games[match_id].launch()
 
     @staticmethod
     def emit(event: str, data=None, room=None, to=None, skip_sid=None):
@@ -67,51 +93,12 @@ class Server:
         Server._loop_lock.release()
 
     @staticmethod
-    def monitoring_routine():
-        log = False
-        '''setting all matches in DB as finished'''
-        matches = fetch_matches()
-        for match in matches:
-            if match.finished == False:
-                match.finish_match()
-        while True:
-            Server._games_lock.acquire()
-            try:
-                to_pop = []
-                for match_code, game in Server._games.items():
-                    if game.check_zombie() == True:
-                        to_pop.append(match_code)
-                        print(f'match {match_code} has been popped of the server', flush=True)
-                for match_code in to_pop:
-                    Server._games.pop(match_code)
-            except RuntimeError:
-                pass
-            Server._games_lock.release()
-            time.sleep(0.1)
-            # Flush stdout to print help aiohttp print its things
-            if log == False:
-                print('', flush=True)
-                log = True
-
-    @staticmethod
-    def launch_monitoring():
-        Thread(target=Server.monitoring_routine).start()
-
-    @staticmethod
-    def delete_game(match_id):
-        # TODO -> set game as finished (django endpoint)
-        Server._games_lock.acquire()
-        Server._games.pop(match_id)
-        Server._games_lock.release()
-
-    @staticmethod
     def get_player_and_match_id(user_id: int):
         Server._games_lock.acquire()
         for match_id in Server._games:
             for team in Server._games[match_id].match.teams:
                 for player in team.players:
-                    print(player.user_id, flush=True)
-                    if player.user_id == id:
+                    if player.user_id == user_id:
                         Server._games_lock.release()
                         return player, match_id
         Server._games_lock.release()
