574fc84772a7675615ff93fe1b7089f01cfa0d9c
fguirama <florian.guiramand@protonmail.com>
2024-12-18 18:56:33 +0100

    fix EventSerializer field

--- srcs/requirements/users/django/events/serializer.py
+++ srcs/requirements/users/django/events/serializer.py
@@ -1,4 +1,7 @@
+import json
+
 import redis
+from lib_transcendence.exceptions import ServiceUnavailable
 from rest_framework import serializers
 
 redis_client = redis.StrictRedis(host='redis')
@@ -6,8 +9,8 @@ redis_client = redis.StrictRedis(host='redis')
 
 class EventSerializer(serializers.Serializer):
     user_id = serializers.IntegerField()
-    type = serializers.CharField(max_length=10)
-    service = serializers.CharField(max_length=10)
+    type = serializers.CharField(max_length=20)
+    service = serializers.CharField(max_length=20)
     message = serializers.CharField(max_length=100)
     code = serializers.CharField(max_length=5, required=False)
 
@@ -26,10 +29,10 @@ class EventSerializer(serializers.Serializer):
     def create(self, validated_data):
         # Publier la notification dans Redis avec user_id
         notification = {
-            "service": user_id,
-            "message": message
+            "message": validated_data['message'],
         }
-        channel = f' :user_{user_id}'
+        user_id = validated_data['user_id']
+        channel = f'events:user_{user_id}'
         try:
             redis_client.publish(channel, json.dumps(notification))
         except redis.exceptions.ConnectionError:
