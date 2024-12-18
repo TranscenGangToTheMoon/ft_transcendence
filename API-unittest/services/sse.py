6e77c6a1734f143106f67632f45a0a8181439a28
fguirama <florian.guiramand@protonmail.com>
2024-12-18 18:55:40 +0100

    remake sse service request

--- API-unittest/services/sse.py
+++ API-unittest/services/sse.py
@@ -1,11 +1,13 @@
+from typing_extensions import Literal
+
 from utils.request import make_request
 
 
-def notification(user_to, message):
+def events(user_to, message, type: Literal['notification'] = 'notification', service: Literal['messages'] = 'messages'):
     return make_request(
-        endpoint='private/users/notification/',
+        endpoint='private/users/events/',
         method='POST',
-        data={'user_id': user_to['id'], 'message': message},
+        data={'user_id': user_to['id'], 'message': message, 'type': type, 'service': service},
         port=8005,
         token=user_to['token'],
     )
