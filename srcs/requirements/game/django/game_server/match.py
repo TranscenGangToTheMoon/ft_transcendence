212025e5be22155586811132b7991d7ee35e1b19
Basile Jeannot <bajeanno@student.42lyon.fr>
2024-12-19 17:49:35 +0100

    added str overload for players and teams

--- srcs/requirements/game/django/game_server/match.py
+++ srcs/requirements/game/django/game_server/match.py
@@ -22,15 +22,22 @@ class Player():
         self.team = team
         self.csc = 0
 
+    def __str__(self):
+        return str(self.model.user_id)
+
 class Team():
     def __init__(self, model, match_id):
         self.match_id = match_id
+        self.id = model.id
         self.players: List[Player] = []
         self.model = model
         self.score = 0
         raw_players = self.model.players.all()
         for player in raw_players:
             self.players.append(Player(player, self.match_id, self))
+
+    def __str__(self):
+        return str(self.id + '[' + str(self.players) + ']')
 
 class Match():
     def __init__(self, match_id):
@@ -43,13 +50,12 @@ class Match():
             self.teams.append(Team(team, self.id))
 
     def __str__(self):
-        return self.model.id + ' ' + str(self.teams[0].model.players.all()) + ' ' + \
-        str(self.teams[1].model.players.all())
+        return str(self.id + '[' + 'Team a: ' + str(self.teams[0]) + ', Team b: ' + str(self.teams[1]) + ']')
 
 def fetch_match(match_id):
-    #print(f'fetching match {match_id}', flush=True)
+    print(f'fetching match {match_id}', flush=True)
     match = Match(match_id)
-    #print(str(match))
+    print(f'fetched match {str(match)}', flush=True)
     return match
 
 async def fetch_match_async(match_id):
