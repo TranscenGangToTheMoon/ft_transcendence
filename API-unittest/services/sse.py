abb531e54cb0adf6d6ba389e14a7aac9aee95e77
fguirama <florian.guiramand@protonmail.com>
2024-12-21 17:34:07 +0100

    handle users in events()

--- API-unittest/services/sse.py
+++ API-unittest/services/sse.py
@@ -3,11 +3,16 @@ from typing_extensions import Literal
 from utils.request import make_request
 
 
-def events(user_to, message, type: Literal['notification'] = 'notification', service: Literal['chat'] = 'chat', event_code: Literal['send-message'] = 'send-message'):
+def events(user_to=None, users=None, data=None, service: Literal['chat'] = 'chat', event_code: Literal['send-message'] = 'send-message'):
+    if users is None:
+        users = []
+    if user_to is not None:
+        users.append(user_to['id'])
+    else:
+        users.append(1)
     return make_request(
         endpoint='private/users/events/',
         method='POST',
-        data={'users_id': [user_to['id']], 'data': {'message': message}, 'type': type, 'event_code': event_code, 'service': service},
+        data={'users_id': users, 'data': data, 'event_code': event_code, 'service': service},
         port=8005,
-        token=user_to['token'],
     )
