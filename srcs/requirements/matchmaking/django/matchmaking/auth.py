e18e607b0b3cdc566b40bc97b2e980df60910140
fguirama <florian.guiramand@protonmail.com>
2024-12-21 18:01:58 +0100

    authenticate with users me and not verify in matchmaking service

---
+++ srcs/requirements/matchmaking/django/matchmaking/auth.py
@@ -0,0 +1,12 @@
+from lib_transcendence import endpoints
+from lib_transcendence.auth import AbstractAuthentication
+from lib_transcendence.exceptions import MessagesException
+from lib_transcendence.services import request_users
+from rest_framework.exceptions import AuthenticationFailed
+
+
+class UserMeAuthentication(AbstractAuthentication):
+    def auth_request(self, token):
+        result = request_users(endpoints.Users.me, token=token)
+        if not result['is_online']:
+            raise AuthenticationFailed(MessagesException.AuthenticationFailed.NOT_CONNECTED_SSE)
--- srcs/requirements/matchmaking/django/matchmaking/settings.py
+++ srcs/requirements/matchmaking/django/matchmaking/settings.py
@@ -144,7 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'lib_transcendence.auth.Authentication',
+        'matchmaking.auth.UserMeAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20,
