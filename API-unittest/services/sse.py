73bfd4e52a927a6ed72c83bf4c0d6b907c142731
fguirama <florian.guiramand@protonmail.com>
2024-12-21 15:35:27 +0100

    fix events() function api-unittest according to new handling of event

--- API-unittest/services/sse.py
+++ API-unittest/services/sse.py
@@ -3,11 +3,11 @@ from typing_extensions import Literal
 from utils.request import make_request
 
 
-def events(user_to, message, type: Literal['notification'] = 'notification', service: Literal['messages'] = 'messages'):
+def events(user_to, message, type: Literal['notification'] = 'notification', service: Literal['chat'] = 'chat', event_code: Literal['send-message'] = 'send-message'):
     return make_request(
         endpoint='private/users/events/',
         method='POST',
-        data={'user_id': user_to['id'], 'message': message, 'type': type, 'service': service},
+        data={'user_id': user_to['id'], 'data': {'message': message}, 'type': type, 'service': service, 'event_code': event_code},
         port=8005,
         token=user_to['token'],
     )
