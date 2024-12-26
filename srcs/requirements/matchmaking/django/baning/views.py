7fdb1589b71bcacf3e37bf0955e16fd39b987991
fguirama <florian.guiramand@protonmail.com>
2024-12-26 19:46:20 +0100

    move baning views to baning app

---
+++ srcs/requirements/matchmaking/django/baning/views.py
@@ -0,0 +1,41 @@
+from django.core.exceptions import PermissionDenied
+from rest_framework import generics
+from lib_transcendence.exceptions import MessagesException
+
+from baning.utils import ban_yourself, get_participants_for_baning, banned
+from lobby.serializers import LobbyParticipantsSerializer
+from matchmaking.utils.participant import get_lobby_participant, get_tournament_participant
+from matchmaking.utils.place import get_lobby, get_tournament
+from tournament.serializers import TournamentParticipantsSerializer
+
+
+class LobbyBanView(generics.DestroyAPIView):
+    serializer_class = LobbyParticipantsSerializer
+
+    def get_object(self):
+        ban_yourself(self.kwargs['user_id'], self.request.user.id)
+        lobby = get_lobby(self.kwargs['code'])
+        get_lobby_participant(lobby, self.request.user.id, True)
+        return get_participants_for_baning(lobby, self.kwargs['user_id'])
+
+    def perform_destroy(self, instance):
+        banned(instance, self.request)
+        super().destroy(instance)
+
+
+class TournamentBanView(generics.DestroyAPIView):
+    serializer_class = TournamentParticipantsSerializer
+
+    def get_object(self):
+        ban_yourself(self.kwargs['user_id'], self.request.user.id)
+        tournament = get_tournament(code=self.kwargs.get('code'))
+        get_tournament_participant(tournament, self.request.user.id, True)
+
+        if tournament.is_started:
+            raise PermissionDenied(MessagesException.PermissionDenied.BAN_AFTER_START)
+
+        return get_participants_for_baning(tournament, self.kwargs['user_id'])
+
+
+lobby_ban_view = LobbyBanView.as_view()
+tournament_ban_view = TournamentBanView.as_view()
