dd0c5ce5a95ea7cb448e18cc3009411fcb068378
fguirama <florian.guiramand@protonmail.com>
2024-12-26 12:53:44 +0100

    remove useless STATICFILES_DIRS

--- srcs/requirements/matchmaking/django/matchmaking/settings.py
+++ srcs/requirements/matchmaking/django/matchmaking/settings.py
@@ -132,7 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Commenting those lines to silence the warning
 # STATIC_ROOT = BASE_DIR / "staticfiles"
