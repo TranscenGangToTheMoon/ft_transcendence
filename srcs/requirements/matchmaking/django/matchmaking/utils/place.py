913511d754f5e1354adbe34d9d715fb8b556b4dc
fguirama <florian.guiramand@protonmail.com>
2024-12-26 19:45:57 +0100

    make utils folder with all utils function

---
+++ srcs/requirements/matchmaking/django/matchmaking/utils/place.py
@@ -0,0 +1,63 @@
+from rest_framework import serializers
+from rest_framework.exceptions import NotFound, PermissionDenied
+from lib_transcendence.exceptions import MessagesException, ResourceExists
+
+from baning.utils import is_banned
+from blocking.utils import are_users_blocked
+from lobby.models import Lobby, LobbyParticipants
+from matchmaking.utils.user import verify_user
+from tournament.models import Tournaments, TournamentParticipants
+
+
+def get_place(model, create, **kwargs):
+    name = type(model).__name__
+    allowed_keys = ('id', 'code')
+
+    key = list(kwargs)[0]
+    assert len(kwargs) == 1 and key in allowed_keys
+
+    value = kwargs[key]
+    if value is None:
+        raise serializers.ValidationError({key: [MessagesException.ValidationError.REQUIRED.format(obj=f'{name.title()} {key}')]})
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        if create:
+            raise NotFound(MessagesException.NotFound.NOT_FOUND.format(obj=name.title()))
+        raise PermissionDenied(MessagesException.PermissionDenied.NOT_BELONG.format(obj=name))
+
+
+def get_lobby(code, create=False):
+    return get_place(Lobby, create, code=code)
+
+
+def get_tournament(create=False, **kwargs):
+    return get_place(Tournaments, create, **kwargs)
+
+
+def verify_place(user, model):
+    name = type(model).__name__
+
+    def get_place_creator():
+        if isinstance(model, Tournaments):
+            return model.created_by
+        else:
+            try:
+                return model.participants.get(creator=True).user_id
+            except (LobbyParticipants.DoesNotExist, TournamentParticipants.DoesNotExist):
+                raise NotFound(MessagesException.NotFound.CREATOR)
+
+    if model.participants.filter(user_id=user['id']).exists():
+        raise ResourceExists(MessagesException.ResourceExists.JOIN.format(obj=name))
+
+    if is_banned(model.code, user['id']) and are_users_blocked(user['id'], get_place_creator()):
+        raise NotFound(MessagesException.NotFound.NOT_FOUND.format(obj=name.title()))
+
+    if isinstance(model, Tournaments) and model.is_started:
+        raise PermissionDenied(MessagesException.PermissionDenied.TOURNAMENT_ALREADY_STARTED)
+
+    verify_user(user['id'])
+
+    if model.is_full:
+        raise PermissionDenied(MessagesException.PermissionDenied.IS_FULL.format(obj=name.title()))
+
---
+++ srcs/requirements/matchmaking/django/matchmaking/utils/sse.py
@@ -0,0 +1,20 @@
+from lib_transcendence.services import create_sse_event
+from lib_transcendence.users import retrieve_users
+from lib_transcendence.sse_events import EventCode
+
+from lobby.models import LobbyParticipants
+from tournament.models import TournamentParticipants
+
+
+def send_sse_event(event: EventCode, instance: LobbyParticipants | TournamentParticipants, data=None, request=None, exclude_myself=True):
+    if data is None:
+        data = {}
+    members = instance.place.participants
+    if exclude_myself:
+        members = members.exclude(id=instance.id)
+    other_members = list(members.values_list('user_id', flat=True))
+    if other_members:
+        if request is not None:
+            user_instance = retrieve_users(instance.user_id, request)
+            data.update(user_instance[0])
+        create_sse_event(other_members, event, data=data)
