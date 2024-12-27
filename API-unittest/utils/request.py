b0de864a4a380ebb51778a7605a0e84b428d6511
fguirama <florian.guiramand@protonmail.com>
2024-12-27 18:49:58 +0100

    fix print request

--- API-unittest/utils/request.py
+++ API-unittest/utils/request.py
@@ -30,7 +30,7 @@ def make_request(endpoint, method: Literal['GET', 'POST', 'DELETE', 'PATCH', 'PU
         verify=False,
     )
 
-    print(f'{method} {endpoint} => {r.status_code}' + ('' if data else f' - {data}'), flush=True)
+    print(f'{method} {endpoint} => {r.status_code}' + (f' - {data}' if data else ''), flush=True)
 
     if r.status_code == 204:
         print()
