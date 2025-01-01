3ba06c8d8d652ad5e5651c8fcbb0f5119609f6b1
fguirama <florian.guiramand@protonmail.com>
2025-01-01 23:30:05 +0100

    send sse event 'tournament-join' when a player join a tournament

--- srcs/requirements/matchmaking/django/tournament/views.py
+++ srcs/requirements/matchmaking/django/tournament/views.py
@@ -1,12 +1,10 @@
-from datetime import datetime, timezone
 from typing import Literal
 
 from django.db.models import Q
 from lib_transcendence.exceptions import MessagesException
 from lib_transcendence.permissions import GuestCannotCreate
 from lib_transcendence.serializer import SerializerAuthContext
-from lib_transcendence.services import request_game
-from lib_transcendence import endpoints
+from lib_transcendence.sse_events import EventCode
 from rest_framework import generics, serializers, status
 from rest_framework.exceptions import PermissionDenied, NotFound
 from rest_framework.permissions import AllowAny
@@ -14,9 +12,9 @@ from rest_framework.response import Response
 
 from blocking.models import Blocked
 from blocking.utils import create_player_instance, delete_player_instance
-from matchmaking.create_match import create_tournament_match
 from matchmaking.utils.participant import get_tournament_participant
 from matchmaking.utils.place import get_tournament
+from matchmaking.utils.sse import send_sse_event
 from tournament.models import Tournament, TournamentParticipants
 from tournament.serializers import TournamentSerializer, TournamentParticipantsSerializer, TournamentSearchSerializer, TournamentResultMatchSerializer
 
@@ -61,22 +59,31 @@ class TournamentParticipantsView(SerializerAuthContext, generics.ListCreateAPIVi
     pagination_class = None
 
     def filter_queryset(self, queryset):
-        tournament = get_tournament(code=self.kwargs.get('code'))
+        tournament = get_tournament(code=self.kwargs['code'])
         queryset = queryset.filter(tournament_id=tournament.id)
         if not queryset.filter(user_id=self.request.user.id).exists():
             raise PermissionDenied(MessagesException.PermissionDenied.NOT_BELONG_TOURNAMENT)
         return queryset
 
     def get_object(self):
-        return get_tournament_participant(get_tournament(code=self.kwargs.get('code')), self.request.user.id)
+        return get_tournament_participant(get_tournament(code=self.kwargs['code']), self.request.user.id)
 
     def create(self, request, *args, **kwargs):
         serializer_participant = TournamentParticipantsSerializer(data=request.data, context=self.get_serializer_context())
         if serializer_participant.is_valid():
-            instance = serializer_participant.save()
-            serializer_tournament = TournamentSerializer(instance.tournament, context=self.get_serializer_context())
+            self.perform_create(serializer_participant)
+            serializer_tournament = TournamentSerializer(serializer_participant.instance.tournament, context=self.get_serializer_context())
             return Response(serializer_tournament.data, status=status.HTTP_201_CREATED)
         return Response(serializer_participant.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        send_sse_event(EventCode.TOURNAMENT_JOIN, serializer.instance, serializer.data, self.request)
+
+        # if tournament.size == tournament.participants.count():
+        #     tournament.start() # todo make
+        # if tournament.start_at is None and int(tournament.size * (80 / 100)) < tournament.participants.count(): todo make
+        #     Thread(target=tournament.start_timer).start()
 
 
 class TournamentResultMatchView(generics.CreateAPIView):
