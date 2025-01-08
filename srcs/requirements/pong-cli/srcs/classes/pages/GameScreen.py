3f074975c291e7d443fa2512110873e87dffe18d
xcharra <xcharra@student.42lyon.fr>
2025-01-08 18:46:53 +0100

    Start to implement socketio communication Delete main socketio exemple

--- srcs/requirements/pong-cli/srcs/classes/pages/GameScreen.py
+++ srcs/requirements/pong-cli/srcs/classes/pages/GameScreen.py
@@ -3,12 +3,12 @@ import asyncio
 import threading
 import time
 
+import socketio
 from pynput import keyboard
 
 # Textual imports
 from textual            import work
 from textual.app        import ComposeResult
-from textual.geometry import Offset
 from textual.screen     import Screen
 from textual.widgets    import Header, Button, Footer
 
@@ -17,7 +17,8 @@ from classes.game.BallWidget        import Ball
 from classes.game.PaddleWidget      import Paddle
 from classes.game.PlaygroundWidget  import Playground
 from classes.utils.config           import Config
-
+from classes.utils.user             import User
+from classes.utils.config           import SSL_CRT
 
 
 class GamePage(Screen):
@@ -33,6 +34,7 @@ class GamePage(Screen):
         self.running = True
         self.key_thread = threading.Thread(target=self.checkKeys)
         self.listener = None
+        self.sio = socketio.Client(ssl_verify=False)
 
     def on_mount(self):
         #key handling
@@ -43,7 +45,10 @@ class GamePage(Screen):
             on_release=self.onRelease)
         self.listener.start()
         #game handling
+        self.eventFromServer()
+        self.setHandler()
         self.gameLoop()
+
 
     def compose(self) -> ComposeResult:
         yield Header()
@@ -77,10 +82,10 @@ class GamePage(Screen):
 
     def checkKeys(self):
         while self.running:
-            if 'w' in self.pressedKeys:
-                self.paddleLeft.moveUp()
-            if 's' in self.pressedKeys:
-                self.paddleLeft.moveDown()
+            # if 'w' in self.pressedKeys:
+            #     self.paddleLeft.moveUp()
+            # if 's' in self.pressedKeys:
+            #     self.paddleLeft.moveDown()
             if keyboard.Key.up in self.pressedKeys:
                 self.paddleRight.moveUp()
             if keyboard.Key.down in self.pressedKeys:
@@ -109,17 +114,90 @@ class GamePage(Screen):
         # pass
         while self.running:
             # print("Loop")
-            self.ball.move()
+            # self.ball.move()
             self.checkWallBounce()
-            self.checkPaddleBounce(self.paddleLeft)
-            self.checkPaddleBounce(self.paddleRight)
+            # self.checkPaddleBounce(self.paddleLeft)
+            # self.checkPaddleBounce(self.paddleRight)
             await asyncio.sleep(1/60)
-            print(f"{self.ball.offset}")
+            # print(f"{self.ball.offset}")
 
-    @work
-    async def eventFromServer(self):
-        pass
+    def eventFromServer(self):
+        try:
+            self.sio.connect(
+                "wss://localhost:4443/",
+                socketio_path="/ws/game/",
+                transports=["websocket"],
+                auth={
+                    "id": User.id,
+                    "token": User.accessToken
+                }
+            )
+            print("Connected to server!")
+        except Exception as error:
+            print(f"From event: {error}")
+
+    def setHandler(self):
+        @self.sio.on('connect')
+        def connect():
+            print("Connected to server event!")
+            print(self.running)
+
+        @self.sio.on('start_game')
+        def start_game_action():
+            print("start_game_action")
+            print(self.running)
+
+        @self.sio.on('start_countdown')
+        def start_countdown_action():
+            print("start_countdown_action")
+            print(self.running)
+
+        @self.sio.on('game_state')
+        def game_state_action(data):
+            print(f"game_state_action: {data}")
+            print(self.running)
+
+        @self.sio.on('connect_error')
+        def connect_error_action():
+            print("connect_error_action")
+            print(self.running)
+
+        @self.sio.on('move_up')
+        def move_up_action(data):
+            print(f"move_up_action: {data}")
+            print(self.running)
+
+        @self.sio.on('move_down')
+        def move_down_action(data):
+            print(f"move_down_action: {data}")
+            print(self.running)
+
+        @self.sio.on('stop_moving')
+        def stop_moving_action(data):
+            print(f"stop_moving_action: {data}")
+            print(self.running)
+
+        @self.sio.on('score')
+        def score_action(data):
+            print(f"score_action: {data}")
+            print(self.running)
+
+        @self.sio.on('game_over')
+        def game_over_action(data):
+            print(f"game_over_action: {data}")
+            print(self.running)
 
     def on_unmount(self) -> None:
         self.running = False
         self.listener.stop()
+
+    # socket.on('connect', connect_action)
+    # socket.on('start_game', start_game_action)
+    # socket.on('start_countdown', start_countdown_action)
+    # socket.on('game_state', game_state_action)
+    # socket.on('connect_error', connect_error_action)
+    # socket.on('move_up', move_up_action)
+    # socket.on('move_down', move_down_action)
+    # socket.on('stop_moving', stop_moving_action)
+    # socket.on('score', score_action)
+    # socket.on('game_over', game_over_action)
