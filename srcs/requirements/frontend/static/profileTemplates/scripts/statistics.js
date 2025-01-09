430e1f08663b377a97e7326928de9026d2d81612
limastic <faust.nino@gmail.com>
2025-01-09 10:51:23 +0100

    waiting for api push

--- srcs/requirements/frontend/static/profileTemplates/scripts/statistics.js
+++ srcs/requirements/frontend/static/profileTemplates/scripts/statistics.js
@@ -1,6 +1,6 @@
 async function loadGameStats(){
     try {
-        let data = apiRequest(getAccessToken(), `${baseAPIUrl}/users/me/stats/`);
+        let data = await apiRequest(getAccessToken(), `${baseAPIUrl}/users/me/stats/`);
         console.log(data);
     }
     catch (error){
