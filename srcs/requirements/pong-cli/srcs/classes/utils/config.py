10033b92ac2159cfb62bb8f2d1a0661f3b05ed8e
xcharra <xcharra@student.42lyon.fr>
2025-01-09 18:50:33 +0100

    Add some config

--- srcs/requirements/pong-cli/srcs/classes/utils/config.py
+++ srcs/requirements/pong-cli/srcs/classes/utils/config.py
@@ -3,16 +3,25 @@ class Config:
         width = 0
         height = 0
     class Playground:
+        cWidth = 800
+        cHeight = 600
         width = 114
         height = 40
     class Paddle:
+        cGap = 100
+        cWidth = 30
+        cHeight = 200
+        cSpeed = 500
         gap = 14
         width = 4
         height = 13
-        speed = 2
+        speed = 33
     class Ball:
         speed = 1 #convert
         width = 4
         height = 2
-
+    class Cell:
+        width = 7
+        height = 15
+    frameRate = 60
 SSL_CRT = "./ft_transcendence.crt"
