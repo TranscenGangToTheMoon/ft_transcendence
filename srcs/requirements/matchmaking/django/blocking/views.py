5250de542bccaaed0dbf2c45eda57a485d2f1783
fguirama <florian.guiramand@protonmail.com>
2025-01-12 00:54:33 +0100

    fix matchmaking blocking

---
+++ srcs/requirements/matchmaking/django/blocking/views.py
@@ -0,0 +1,21 @@
+from lib_transcendence.exceptions import MessagesException
+from lib_transcendence.serializer import SerializerKwargsContext
+from rest_framework import generics
+from rest_framework.exceptions import NotFound
+
+from blocking.models import Blocked
+from blocking.serializer import BlockedSerializer
+
+
+class BlockedUserView(SerializerKwargsContext, generics.CreateAPIView, generics.DestroyAPIView):
+    serializer_class = BlockedSerializer
+    authentication_classes = []
+
+    def get_object(self):
+        try:
+            return Blocked.objects.get(user_id=self.kwargs['user_id'], blocked_user_id=self.kwargs['blocked_user_id'])
+        except Blocked.DoesNotExist:
+            raise NotFound(MessagesException.NotFound.BLOCKED_INSTANCE)
+
+
+blocked_user_view = BlockedUserView.as_view()
