80cf57aa48481f4894260a5cb5885fc25ae1062c
limastic <faust.nino@gmail.com>
2025-01-13 19:09:18 +0100

    changed the way to access lobbies for guests

--- srcs/requirements/frontend/static/scripts/homePage.js
+++ srcs/requirements/frontend/static/scripts/homePage.js
@@ -9,6 +9,23 @@ document.getElementById('duel').addEventListener('click', async event => {
 
 document.getElementById('clash').addEventListener('click', async event => {
     event.preventDefault();
+    if (userInformations.is_guest){
+        document.getElementById('lobbyCodeContextError').innerText = '';
+        const lobbyCodeSelectionForm = new bootstrap.Modal(document.getElementById('lobbyCodeModal'));
+        lobbyCodeSelectionForm.show();
+        async function handleInput(event){
+            event.preventDefault();
+            let code = document.getElementById('lobbyCodeInput').value;
+            if (code.length != 4 || isNaN(parseInt(code)))
+                return document.getElementById('lobbyCodeContextError').innerText = 'This is not a valid code.';
+            bootstrap.Modal.getOrCreateInstance(document.getElementById('lobbyCodeModal')).hide();
+            setTimeout(async ()=> {
+                await navigateTo(`/lobby/${code}`);
+            }, 100);
+        }
+        document.getElementById('joinLobbyByCode').addEventListener('click', handleInput);
+        return;
+    }
     try {
         let data = await apiRequest(getAccessToken(), `${baseAPIUrl}/play/lobby/`, 'POST', undefined, undefined, {
             'game_mode' : 'clash',
@@ -24,6 +41,23 @@ document.getElementById('clash').addEventListener('click', async event => {
 
 document.getElementById('customGame').addEventListener('click', async event => {
     event.preventDefault();
+    if (userInformations.is_guest){
+        document.getElementById('lobbyCodeContextError').innerText = '';
+        const lobbyCodeSelectionForm = new bootstrap.Modal(document.getElementById('lobbyCodeModal'));
+        lobbyCodeSelectionForm.show();
+        async function handleInput(event){
+            event.preventDefault();
+            let code = document.getElementById('lobbyCodeInput').value;
+            if (code.length != 4 || isNaN(parseInt(code)))
+                return document.getElementById('lobbyCodeContextError').innerText = 'This is not a valid code.';
+            bootstrap.Modal.getOrCreateInstance(document.getElementById('lobbyCodeModal')).hide();
+            setTimeout(async ()=> {
+                await navigateTo(`/lobby/${code}`);
+            }, 100);
+        }
+        document.getElementById('joinLobbyByCode').addEventListener('click', handleInput);
+        return;
+    }
     try {
         let data = await apiRequest(getAccessToken(), `${baseAPIUrl}/play/lobby/`, 'POST', undefined, undefined, {
             'game_mode' : 'custom_game',
