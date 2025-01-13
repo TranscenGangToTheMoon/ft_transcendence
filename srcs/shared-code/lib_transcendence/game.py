50596eee86f059af13c78cee9a511c01d0b32309
limastic <faust.nino@gmail.com>
2025-01-13 15:40:01 +0100

    temporarily fixed player-disconnect game-over

--- srcs/shared-code/lib_transcendence/game.py
+++ srcs/shared-code/lib_transcendence/game.py
@@ -38,7 +38,7 @@ class GameMode:
 class FinishReason:
     NORMAL_END = 'normal-end'
     PLAYER_ABANDON = 'player-abandon'
-    PLAYER_DISCONNECT = 'player-disconnect'
+    PLAYER_DISCONNECT = 'player-abandon'
     PLAYER_NOT_CONNECTED = 'player-not-connected'
 
     @staticmethod
