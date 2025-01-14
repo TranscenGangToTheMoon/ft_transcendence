cde8cbb725a75129e598807751505a819e5348b5
xcharra <xcharra@student.42lyon.fr>
2025-01-14 18:03:44 +0100

    Migrate css to appropriate location

--- srcs/requirements/pong-cli/srcs/classes/modalScreens/CountdownModalScreen.py
+++ srcs/requirements/pong-cli/srcs/classes/modalScreens/CountdownModalScreen.py
@@ -8,30 +8,14 @@ from textual.widgets    import Digits
 from textual.screen     import ModalScreen
 
 class Countdown(ModalScreen):
-    DEFAULT_CSS = """
-        Countdown {
-            align: center middle;
-            background: black 0%;
-        }
-    """
+    CSS_PATH = "styles/CountdownModalScreen.tcss"
+
     def __init__(self):
         super().__init__()
         self.countdown = 3
-    #     # self.styles.align = ("center", "middle")
-    #     # self.styles.width = "auto"
-    #     # self.styles.height = "auto"
-    #     # self.styles.background = "blue 12%"
-    #     # self.styles.border = ("round", "white")
-    #
         self.digits = Digits(str(self.countdown), id="countdown")
-        self.digits.styles.width = 10
-        self.digits.styles.height = "auto"
-        self.digits.styles.content_align = ("center", "middle")
-        self.digits.styles.border = ("round", "white")
-        self.digits.styles.background = "gray"
-        self.digits.styles.padding = (1, 2)
         self.start()
-    #
+
     def compose(self) -> ComposeResult:
         yield self.digits
 
