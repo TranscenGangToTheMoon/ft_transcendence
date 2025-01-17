fb0f24181a2eb26468695692e96eba17efad34c9
xcharra <xcharra@student.42lyon.fr>
2025-01-17 15:38:41 +0100

    Enhance style of Main Page

--- srcs/requirements/pong-cli/srcs/classes/screens/MainScreen.py
+++ srcs/requirements/pong-cli/srcs/classes/screens/MainScreen.py
@@ -4,8 +4,9 @@ import requests
 # Textual imports
 from textual            import on
 from textual.app        import ComposeResult
+from textual.containers import Horizontal
 from textual.screen     import Screen
-from textual.widgets    import Button, Footer, Header, Rule, Static
+from textual.widgets    import Button, Footer, Header, Label, Rule, Static
 
 # Local imports
 from classes.utils.config   import Config
@@ -19,21 +20,27 @@ class MainPage(Screen):
         yield Header()
         yield Footer()
 
-        yield self.userMeStatic()
+        yield Static("" , id="userMeStatic")
         yield Rule()
-        yield Static("", id="duelResult")
-        yield Button("Duel", id="duel", variant="primary")
+        with Horizontal(id="buttonBoxGame"):
+            yield Button("Duel", id="duel", variant="primary")
+            yield Button("Cancel Duel", id="cancelDuelGame", variant="error", disabled=True)
         yield Rule()
-        yield Static("", id="cancelDuelResult")
-        yield Button("Cancel Duel", id="cancelDuelGame", variant="error", disabled=True)
-        yield Rule()
+        yield Label("", id="statusGame")
+
+    def on_mount(self):
+        try:
+            response = requests.get(url=f"{User.server}/api/users/me/", data={}, headers=User.headers, verify=Config.SSL.CRT)
+            User.id = response.json()["id"]
+            self.query_one("#userMeStatic").update(f"Welcolme {response.json()['username']}\n(id: {User.id})")
+        except Exception as error:
+            self.query_one("#userMeStatic").update(f"GET /api/users/me/ Error: {error}")
 
     def on_screen_resume(self) -> None:
         self.query_one("#duel").loading = False
         self.query_one("#duel").variant = "primary"
         self.query_one("#cancelDuelGame").disabled = True
-        self.query_one("#duelResult").update("")
-        self.query_one("#cancelDuelResult").update("")
+        self.query_one("#statusGame").update("")
 
     @on(Button.Pressed, "#duel")
     def duelAction(self):
@@ -44,9 +51,9 @@ class MainPage(Screen):
             self.query_one("#duel").loading = True
             self.query_one("#duel").variant = "default"
             self.query_one("#cancelDuelGame").disabled = False
-            self.query_one("#duelResult").update(f"({response.status_code}) Searching for an opponent")
+            self.query_one("#statusGame").update(f"({response.status_code}) Searching for an opponent")
         except Exception as error:
-            self.query_one("#duelResult").update(f"POST /api/play/duel/ Error: {error}")
+            self.query_one("#statusGame").update(f"POST /api/play/duel/ Error: {error}")
 
     @on(Button.Pressed, "#cancelDuelGame")
     def cancelDuelAction(self):
@@ -55,18 +62,10 @@ class MainPage(Screen):
             if (response.status_code >= 400): #if 404 c'est que j'ai join le match maius oas recu le event SSE
                 raise (Exception(f"({response.status_code}) Error: {response.text}"))
             elif (response.status_code == 204):
-                self.query_one("#cancelDuelResult").update(f"({response.status_code}) DELETE /api/play/duel/ : Duel deleted")
+                self.query_one("#statusGame").update(f"({response.status_code}) Duel deleted")
                 self.query_one("#duel").loading = False
                 self.query_one("#duel").variant = "primary"
                 self.query_one("#cancelDuelGame").disabled = True
 
         except Exception as error:
             self.query_one("#cancelDuelResult").update(f"DELETE /api/play/duel/ Error: {error}")
-
-    def userMeStatic(self) -> Static:
-        try:
-            response = requests.get(url=f"{User.server}/api/users/me/", data={}, headers=User.headers, verify=Config.SSL.CRT)
-            User.id = response.json()["id"]
-            return Static(f"({response.status_code}) GET /api/users/me/ : {response.text}")
-        except Exception as error:
-            return Static(f"GET /api/users/me/ Error: {error}")
