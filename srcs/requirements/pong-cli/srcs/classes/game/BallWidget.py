c606bf81f1a085141549994ed6e5614b361ec04b
xcharra <xcharra@student.42lyon.fr>
2025-01-23 16:25:21 +0100

    Clean BallWidget, change cHeight and CWidth to cSize Delete comment and get random direction

--- srcs/requirements/pong-cli/srcs/classes/game/BallWidget.py
+++ srcs/requirements/pong-cli/srcs/classes/game/BallWidget.py
@@ -1,19 +1,9 @@
-# Python imports
-import math
-import random
-
 # Textual imports
 from textual.geometry   import Offset
 from textual.widget     import Widget
 
 # Local imports
 from classes.utils.config   import Config
-
-def get_random_direction():
-    random_angle = random.random() * 2 * math.pi
-    while abs(math.cos(random_angle)) < math.cos(math.pi / 3):
-        random_angle = random.random() * 2 * math.pi
-    return math.cos(random_angle), math.sin(random_angle)
 
 class Ball(Widget):
     def __init__(self):
@@ -22,8 +12,8 @@ class Ball(Widget):
         self.styles.width = Config.Ball.width
         self.styles.height = Config.Ball.height
         self.styles.background = "white"
-        self.cX = Config.Playground.cWidth / 2 - (Config.Ball.cWidth / 2)
-        self.cY = Config.Playground.cHeight / 2 - (Config.Ball.cHeight / 2)
+        self.cX = Config.Playground.cWidth / 2 - (Config.Ball.cSize / 2)
+        self.cY = Config.Playground.cHeight / 2 - (Config.Ball.cSize / 2)
         self.cdX = 0
         self.cdY = 0
         self.offset = Offset(
@@ -49,4 +39,3 @@ class Ball(Widget):
             int(self.cX  * Config.Playground.width / Config.Playground.cWidth),
             int(self.cY * Config.Playground.height / Config.Playground.cHeight)
         )
-        # print(f"{self.offset} += {Offset(self.dx, self.dy)}")
