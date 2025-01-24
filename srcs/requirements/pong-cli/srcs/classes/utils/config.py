129c1f314c78a02b9031bccb90b0b34c3894048b
xcharra <xcharra@student.42lyon.fr>
2025-01-24 19:54:03 +0100

    Reintroduce Paddle speed and cspeed

--- srcs/requirements/pong-cli/srcs/classes/utils/config.py
+++ srcs/requirements/pong-cli/srcs/classes/utils/config.py
@@ -31,9 +31,11 @@ class Config:
             f"Paddle.cGap:......... {cls.Paddle.cGap}\n"
             f"Paddle.cWidth:....... {cls.Paddle.cWidth}\n"
             f"Paddle.cHeight:...... {cls.Paddle.cHeight}\n"
+            f"Paddle.cSpeed:....... {cls.Paddle.cSpeed}\n"
             f"Paddle.gap:.......... {cls.Paddle.gap}\n"
             f"Paddle.width:........ {cls.Paddle.width}\n"
             f"Paddle.height:....... {cls.Paddle.height}\n"
+            f"Paddle.speed:........ {cls.Paddle.speed}\n"
             f"Ball.cSize:.......... {cls.Ball.cSize}\n"
             f"Ball.width:.......... {cls.Ball.width}\n"
             f"Ball.height:......... {cls.Ball.height}\n"
@@ -72,9 +74,11 @@ class Config:
         cGap: int = 100
         cWidth: int = 30
         cHeight: int = 200
+        cSpeed: int = 500
         gap: int = 14
         width: int = 4
         height: int = 13
+        speed: int = 33
 
         @classmethod
         def load(cls):
@@ -82,9 +86,11 @@ class Config:
                 cls.cGap = Config.get("paddle", "normal", "ledgeOffset", default=100)
                 cls.cWidth = Config.get("paddle", "normal", "width", default=30)
                 cls.cHeight = Config.get("paddle", "normal", "height", default=200)
+                cls.cSpeed = Config.get("paddle", "normal", "speed", default=500)
                 cls.gap = int(cls.cGap / Config.Cell.width)
                 cls.width = int(cls.cWidth / Config.Cell.width)
                 cls.height = int(cls.cHeight / Config.Cell.height)
+                cls.speed = int(cls.cSpeed / Config.Cell.height)
 
     class Ball:
         cSize: int = 20
