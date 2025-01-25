2317376e760feef363bd1771a77fd81453a043f1
fguirama <florian.guiramand@protonmail.com>
2025-01-25 13:46:00 +0100

    fix settings.py

--- srcs/requirements/auth/django/auth/settings.py
+++ srcs/requirements/auth/django/auth/settings.py
@@ -14,7 +14,10 @@ from pathlib import Path
 from os import environ
 
 
+# Own settings
 APPEND_SLASH = False
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,6 +95,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
