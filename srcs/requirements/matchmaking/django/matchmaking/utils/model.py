0c22d0511b5f3d52d41f92e17f27cb13d9820a79
fguirama <florian.guiramand@protonmail.com>
2025-01-28 10:18:11 +0100

    fix ParticipantsPlace

--- srcs/requirements/matchmaking/django/matchmaking/utils/model.py
+++ srcs/requirements/matchmaking/django/matchmaking/utils/model.py
@@ -1,10 +1,8 @@
 class ParticipantsPlace:
     creator = None
     user_id = None
-
-    def __init__(self):
-        self.DoesNotExist = None
-        self.objects = None
+    DoesNotExist = None
+    objects = None
 
     @property
     def place(self):
