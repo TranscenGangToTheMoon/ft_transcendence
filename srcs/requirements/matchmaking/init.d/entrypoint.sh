a04f758deb248dd74984c428a55574d19da2e3c6
fguirama <florian.guiramand@protonmail.com>
2025-01-30 12:02:34 +0100

    fix matchmaking entrypoint.sh

--- srcs/requirements/matchmaking/init.d/entrypoint.sh
+++ srcs/requirements/matchmaking/init.d/entrypoint.sh
@@ -19,9 +19,7 @@ DELETE FROM lobby_lobby;
 DELETE FROM lobby_lobbyparticipants;
 DELETE FROM play_players;
 DELETE FROM tournament_tournament;
-DELETE FROM tournament_tournamentstage;
 DELETE FROM tournament_tournamentparticipants;
-DELETE FROM tournament_tournamentmatches;
 "
 
 exec "$@"
