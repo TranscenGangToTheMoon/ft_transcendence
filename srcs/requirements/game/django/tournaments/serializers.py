e346253bc83c9edd242d1d8c8f74dcc477f983ae
fguirama <florian.guiramand@protonmail.com>
2025-01-30 18:32:47 +0100

    start fix tournament serializers.py

--- srcs/requirements/game/django/tournaments/serializers.py
+++ srcs/requirements/game/django/tournaments/serializers.py
@@ -1,16 +1,26 @@
+from math import log2
+from threading import Thread
+
+from django.db.models.functions import Random
 from rest_framework import serializers
 
 from lib_transcendence.serializer import Serializer
+from lib_transcendence.sse_events import create_sse_event, EventCode
 from lib_transcendence.users import retrieve_users
 from matches.models import Matches
 from matches.serializers import TournamentMatchSerializer
 from tournaments.models import Tournaments
 
 
+class TournamentPlayerSerializer(Serializer):
+    id = serializers.IntegerField()
+    trophies = serializers.IntegerField()
+
+
 class TournamentSerializer(Serializer):
     id = serializers.IntegerField()
-    stages = serializers.ListField(child=serializers.DictField(), write_only=True)
     matches = serializers.SerializerMethodField(read_only=True)
+    participants = serializers.ListField(child=TournamentPlayerSerializer(), write_only=True)
 
     class Meta:
         model = Tournaments
@@ -23,6 +33,10 @@ class TournamentSerializer(Serializer):
             'created_by',
             'matches',
             'stages',
+        ]
+        read_only_fields = [
+            'start_at',
+            'finish_at',
         ]
 
     def get_matches(self, obj):
@@ -42,101 +56,37 @@ class TournamentSerializer(Serializer):
         return matches
 
     def create(self, validated_data):
-        stages = validated_data.pop('stages')
+        participants = validated_data.pop('participants')
         result = super().create(validated_data)
-        for stage in stages:
-            result.stages.create(**stage)
-        self.is_started = True
-        self.start_at = datetime.now(timezone.utc)
-        for n_stage in range(int(log2(self.size))):
-            self.stages.create(label=Tournament.stage_labels[n_stage], stage=n_stage)
-        self.current_stage = self.stages.last()
-        self.save()
-        first_stage = self.current_stage
-        participants = self.participants.all().order_by('-trophies', Random())
+        for user in participants:
+            result.participants.create(user_id=user['id'], trophies=user['trophies'])
+        for n_stage in range(int(log2(result.size))):
+            result.stages.create(label=Tournaments.stage_labels[n_stage], stage=n_stage)
+        result.current_stage = result.stages.last()
+        result.save()
+        first_stage = result.current_stage
+        participants = result.participants.all().order_by('-trophies', Random())
 
         for n, p in enumerate(participants):
             p.seed = n + 1
             p.stage = first_stage
             p.save()
 
-        for i in range(int(self.size / 2)):
+        for i in range(int(result.size / 2)):
             user_1 = participants[i]
-            index = self.match_order[self.size][user_1.seed]
+            index = result.match_order[result.size][user_1.seed]
             user_1.index = index
             user_1.save()
-            k = self.size - i - 1
+            k = result.size - i - 1
             if participants.count() > k:
                 user_2 = participants[k]
                 user_2.index = index
                 user_2.save()
             else:
                 user_2 = None
-            self.matches.create(n=index, stage=first_stage, user_1=user_1, user_2=user_2)
-            self.nb_matches = index
-        self.save()
-        start_tournament_sse(self)
-        time.sleep(3)
-        for matche in self.matches.all():
-            matche.post()
+            result.matches.create(n=index, stage=first_stage, user_1=user_1, user_2=user_2)
+            result.nb_matches = index
+        result.save()
+        create_sse_event(result.users_id(), EventCode.TOURNAMENT_START, TournamentSerializer(result).data, {'name': result.name})
+        Thread(target=result.post_matches).start()
         return result
-
-
-class TournamentStageSerializer(Serializer):
-    class Meta:
-        model = TournamentStage
-        fields = [
-            'id',
-            'label',
-            'stage',
-        ]
-
-
-
-class TournamentParticipantsSerializer(Serializer):
-    id = serializers.IntegerField(source='user_id', read_only=True)
-    tournament = serializers.CharField(max_length=4, source='tournament.code', read_only=True)
-
-    class Meta:
-        model = TournamentParticipants
-        fields = [
-            'id',
-            'tournament',
-            'stage',
-            'seed',
-            'still_in',
-            'creator',
-            'join_at',
-        ]
-        read_only_fields = [
-            'id',
-            'tournament',
-            'stage',
-            'seed',
-            'still_in',
-            'creator',
-            'join_at',
-        ]
-
-    def create(self, validated_data):
-        user = self.context['auth_user']
-        tournament = get_tournament(create=True, code=self.context.get('code'))
-
-        try:
-            user = tournament.participants.get(user_id=user['id'], connected=False)
-            user.reconnect()
-            return user
-        except TournamentParticipants.DoesNotExist:
-            pass
-
-        verify_place(user, tournament)
-
-        if tournament.created_by == user['id']:
-            validated_data['creator'] = True
-        validated_data['user_id'] = user['id']
-        validated_data['trophies'] = user['trophies']
-        validated_data['tournament'] = tournament
-        result = super().create(validated_data)
-        create_player_instance(self.context['request'])
-        return result
-
