e5ecde5f238b84f55bbcdcb6c7685f57f5a5bda4
limastic <faust.nino@gmail.com>
2025-01-31 14:18:34 +0100

    block user from friend list

--- srcs/requirements/frontend/static/friends/scripts/friendList.js
+++ srcs/requirements/frontend/static/friends/scripts/friendList.js
@@ -61,9 +61,24 @@ if (document.getElementById('modals').friendListened !== true){
             console.log(position);
             contextMenu.style.left = `${position.left + position.width}px`;
             contextMenu.style.top = `${position.top}px`;
+            contextMenu.friendId = friendRequestDiv.friendId;
+            console.log(friendRequestDiv);
             setTimeout(()=> {
                 contextMenu.style.display = 'block';
             }, 200);
+        }
+        if (event.target.matches('.cBlock')){
+            const contextMenu = event.target.closest('#friendListContextMenu');
+            console.log(contextMenu.friendId);
+            const friendId = contextMenu.friendId;
+            try {
+                await apiRequest(getAccessToken(), `${baseAPIUrl}/users/me/blocked/`, 'POST', undefined, undefined, {
+                    "user_id" : friendId
+                })
+            }
+            catch(error){
+                console.log(error);
+            }
         }
     })
 }
@@ -112,7 +127,9 @@ async function getMoreFriendRequests() {
             requestDiv.id = `fr${result.id}`;
             friendRequestsDiv.appendChild(requestDiv);
             await loadContent('/friends/friendRequestBlock.html', `${requestDiv.id}`);
-            requestDiv.querySelector(`.senderUsername`).innerText = result.sender.username;
+            requestDiv.querySelector('img').src = result.sender.profile_picture.small;
+            requestDiv.querySelector('.senderUsername').innerText = result.sender.username;
+            requestDiv.querySelector('.friendRequestBlock').friendId = result.sender.id;
         }
     }
     catch (error) {
@@ -220,6 +237,7 @@ async function loadFriendList(){
             <img src="/assets/kebabMenu.png" class="friendRequestKebabMenuButton mx-2"></div>
             </div>\n`;
             const friendDiv = resultDiv.querySelector(`#friend${friend.id}`);
+            friendDiv.friendId = friend.friend.id;
             console.log(friend);
             friendDiv.setAttribute('data-bs-toggle', 'popover');
             friendDiv.setAttribute('data-bs-trigger', 'hover');
@@ -258,6 +276,7 @@ function addFriend(friendInstance){
     <button class='btn btn-danger deleteFriend'>delete</button>
     <img src="/assets/kebabMenu.png" class="friendRequestKebabMenuButton mx-2"></div></div>\n`;
     const friendInstanceDiv = friendDiv.querySelector(`#friend${friendInstance.id}`);
+    friendInstanceDiv.friendId = friendInstance.friend.id;
     friendInstanceDiv.setAttribute('data-bs-toggle', 'popover');
     friendInstanceDiv.setAttribute('data-bs-trigger', 'hover');
     friendInstanceDiv.setAttribute('data-bs-placement', 'top');
@@ -299,6 +318,7 @@ async function addFriendRequest(result){
     await loadContent('/friends/friendRequestBlock.html', `${requestDiv.id}`);
     requestDiv.querySelector('img').src = result.sender.profile_picture.small;
     requestDiv.querySelector('.senderUsername').innerText = result.sender.username;
+    requestDiv.querySelector('.friendRequestBlock').friendId = result.sender.id;
 }
 
 async function removeFriendRequest(id){
@@ -333,6 +353,7 @@ async function loadReceivedFriendRequests(){
             await loadContent('/friends/friendRequestBlock.html', `${requestDiv.id}`);
             requestDiv.querySelector('.senderUsername').innerText = result.sender.username;
             requestDiv.querySelector('img').src = result.sender.profile_picture.small;
+            requestDiv.querySelector('.friendRequestBlock').friendId = result.sender.id;
         }
     }
 }
