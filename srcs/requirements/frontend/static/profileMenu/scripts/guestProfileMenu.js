00573ae3250c55a9061780d44c18b84eee475fa6
limastic <faust.nino@gmail.com>
2025-01-31 10:18:19 +0100

    fixed login form hitbox

--- srcs/requirements/frontend/static/profileMenu/scripts/guestProfileMenu.js
+++ srcs/requirements/frontend/static/profileMenu/scripts/guestProfileMenu.js
@@ -1,3 +1,20 @@
+if (typeof isClickStartingInside === 'undefined')
+    var isClickStartingInside = false;
+
+document.getElementById('loginMenu').addEventListener('click', event => {
+    event.stopPropagation();
+})
+
+document.getElementById('loginMenu').addEventListener('mousedown', event => {
+    isClickStartingInside = true;
+})
+
+document.addEventListener("hide.bs.dropdown", function (event) {
+    if (isClickStartingInside)
+        event.preventDefault();
+    isClickStartingInside = false;
+});
+
 async function guestProfileInit() {
     await loadContent('/profileMenu/authenticateForm/authenticationForm.html', 'authenticate');
 }
